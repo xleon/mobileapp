7a496a71981baad77afd9574887a7b45a22f4662
William Barbosa <william.sb@hotmail.com>
2017-04-03 10:50:56 -0300

    :white_check_mark: Add test projects

---
+++ Toggl.Ultrawave.Tests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Toggl.Ultrawave.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
