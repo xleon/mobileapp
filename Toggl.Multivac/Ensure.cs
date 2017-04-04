d7143a10163fd79c625d8f01f9e960697e864eb8
William Barbosa <william.sb@hotmail.com>
2017-04-04 13:18:11 -0300

    :computer: Add Multivac project (#4)

---
+++ Toggl.Multivac/Ensure.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Toggl.Multivac
+{
+	public static class Ensure
+	{
+		public static void ArgumentIsNotNull<T>(T value, string argumentName)
+			where T : class
+		{
+			if (value != null) return;
+
+			throw new ArgumentNullException(argumentName);
+		}
+	}
+}
