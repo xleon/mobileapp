9d375b6bb048ff2a06ac2d69f3d4f6555bf1dd58
William Barbosa <william.sb@hotmail.com>
2017-04-04 11:48:14 -0300

    :couple: Expose Ultrawave internals to test assemblies (#3)

--- Toggl.Ultrawave/Properties/AssemblyInfo.cs
+++ Toggl.Ultrawave/Properties/AssemblyInfo.cs
@@ -12,6 +12,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: InternalsVisibleTo("Toggl.Ultrawave.Tests")]
+[assembly: InternalsVisibleTo("Toggl.Ultrawave.Tests.Integration")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
