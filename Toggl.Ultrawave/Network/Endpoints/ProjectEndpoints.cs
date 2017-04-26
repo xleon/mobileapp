252a146b5434c57f9034e52735864f226ff7fb65
Artūrs Eimanis <arturseimanis@gmail.com>
2017-04-26 09:20:55 +0300

    Add Project endpoints (#66)
    * :construction: Add Project endpoints

---
+++ Toggl.Ultrawave/Network/Endpoints/ProjectEndpoints.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Toggl.Ultrawave.Network
+{
+    internal struct ProjectEndpoints
+    {
+        private readonly Uri baseUrl;
+
+        public ProjectEndpoints(Uri baseUrl)
+        {
+        	this.baseUrl = baseUrl;
+        }
+
+        public Endpoint Get => Endpoint.Get(baseUrl, "me/projects");
+
+        public Endpoint Post(int workspaceId)
+            => Endpoint.Post(baseUrl, $"workspaces/{ workspaceId }/projects");
+    }
+}
