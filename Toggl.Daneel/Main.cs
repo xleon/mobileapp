f59afc67c3c0e3f80df250604f07cde622b19d13
William Barbosa <william.sb@hotmail.com>
2017-04-26 14:31:02 +0200

    :apple: Add iOS projects (#61)

---
+++ Toggl.Daneel/Main.cs
@@ -0,0 +1,10 @@
+﻿using UIKit;
+
+namespace Toggl.Daneel
+{
+    public class Application
+    {
+        public static void Main(string[] args)
+            => UIApplication.Main(args, null, "AppDelegate");
+    }
+}
---
+++ Toggl.Daneel/ViewController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using UIKit;
+
+namespace Toggl.Daneel
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) 
+            : base(handle)
+        {
+        }
+    }
+}
