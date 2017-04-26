60ae0db6ec4ca7082ddc5ada6628cdd5ccec0bf4
William Barbosa <william.sb@hotmail.com>
2017-04-26 10:52:42 +0200

    :white_check_mark: UserClient Integration tests (#57)
    * :package: Add needed references
    
    * :construction: Add the means to create a new user
    
    * :white_check_mark: Integration tests for UserClient
    
    * :construction: Change usage of the CreateUser method
    
    * :construction: Use value tuples in integration tests (#70)
    
    * :construction: Make tests async
    
    * :japanese_goblin: I SWEAR TO GOD XAMARIN STUDIO
    
    * :lipstick: Add line between Arrange and act

---
+++ Toggl.Ultrawave.Tests.Integration/Helper/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toggl.Ultrawave.Tests.Integration
+{
+    internal static class User
+    {
+        public static async Task<(string email, string password)> Create()
+        {
+            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+
+            var email = $"{Guid.NewGuid()}@mocks.toggl.com";
+            var password = "123456";
+
+            using (var client = new HttpClient())
+            {
+                var json = $"{{\"user\":{{\"email\":\"{email}\",\"password\":\"{password}\"}}}}";
+                var url = new Uri("https://toggl.space/api/v8/signups");
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                await client.PostAsync(url, content);
+            }
+
+            return (email, password);
+        }
+    }
+}
---
+++ Toggl.Ultrawave.Tests.Integration/UserClientTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Toggl.Ultrawave.Clients;
+using Xunit;
+
+namespace Toggl.Ultrawave.Tests.Integration
+{
+    public class UserClientTests
+    {
+        public class TheGetMethod
+        {
+            private readonly IUserClient userClient = new TogglClient(ApiEnvironment.Staging).User;
+
+            [Fact]
+            public async Task WorksForExistingUsers()
+            {
+                var (email, password) = await User.Create();
+
+                var response = await userClient.Get(email, password).Execute();
+
+                response.Success.Should().BeTrue();
+            }
+
+            [Fact]
+            public async Task FailsForNonExistingEmails()
+            {
+                var response = await userClient.Get($"some-non-existing-email-{Guid.NewGuid()}@ironicmocks.toggl.com", "123456789").Execute();
+
+                response.Success.Should().BeFalse();
+                //TODO: Include check expected error message/code
+            }
+
+            [Fact]
+            public async Task FailsIfUsingTheWrongPassword()
+            {
+                var (email, password) = await User.Create();
+
+                var response = await userClient.Get(email, "123457").Execute();
+
+                response.Success.Should().BeFalse();
+                //TODO: Include check expected error message/code
+            }
+        }
+    }
+}
