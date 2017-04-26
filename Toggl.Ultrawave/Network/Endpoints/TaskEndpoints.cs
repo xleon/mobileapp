2491575469f7188bd79f6052a1477c8af806a05e
Artūrs Eimanis <arturseimanis@gmail.com>
2017-04-26 09:16:57 +0300

    Add Task endpoints (#67)
    * :construction: Add Task endpoints

---
+++ Toggl.Ultrawave/Network/Endpoints/TaskEndpoints.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Toggl.Ultrawave.Network
+{
+    internal struct TaskEndpoints
+    {
+        private readonly Uri baseUrl;
+
+        public TaskEndpoints(Uri baseUrl)
+        {
+        	this.baseUrl = baseUrl;
+        }
+
+        public Endpoint Get => Endpoint.Get(baseUrl, "me/tasks");
+    }
+}
