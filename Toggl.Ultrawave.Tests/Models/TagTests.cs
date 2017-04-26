95c399c61e0dd47ec57f17c031ef6e86e9deb60d
Artūrs Eimanis <arturseimanis@gmail.com>
2017-04-26 09:24:37 +0300

    :construction: Add Tag model (#64)
    * :construction: Add Tag model
    
    * :white_check_mark: Add TagTests

---
+++ Toggl.Ultrawave.Tests/Models/TagTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Toggl.Ultrawave.Tests.Models
+{
+    public class TagTests : BaseModelTests<Tag>
+    {
+        protected override string ValidJson
+            => "{\"id\":2024667,\"wid\":424213,\"name\":\"mobile\"}";
+
+        protected override Tag ValidObject => new Tag
+        {
+            Id = 2024667,
+            WorkspaceId = 424213,
+            Name = "mobile"
+        };
+    }
+}
---
+++ Toggl.Ultrawave/Models/Tag.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace Toggl.Ultrawave
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+
+        [JsonProperty("wid")]
+        public int WorkspaceId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
