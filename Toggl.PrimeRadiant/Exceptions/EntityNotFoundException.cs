9621c4c98851a6bec717d7c605fbb15a51fafc10
William Barbosa <william.sb@hotmail.com>
2017-05-09 03:38:49 -0300

    :construction: Add Exceptions to PrimeRadiant (#108)

---
+++ Toggl.PrimeRadiant/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Toggl.PrimeRadiant.Exceptions
+{
+    public class EntityNotFoundException : DatabaseException
+    {
+        private const string defaultErrorMessage = "Entity not found in database";
+
+        public EntityNotFoundException()
+            : this(defaultErrorMessage)
+        {
+        }
+
+        public EntityNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public EntityNotFoundException(InvalidOperationException ex)
+            : base(defaultErrorMessage, ex)
+        {
+        }
+    }
+}
