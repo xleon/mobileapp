2a71a559f4d345844042ecfca94fbbb7d139ab81
Paul Scharf <amul@amulware.net>
2017-05-11 15:04:29 +0200

    :white_check_mark: Abstract endpoint authentication tests (#114)

---
+++ Toggl.Ultrawave.Tests.Integration/AuthenticatedEndpointBaseTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Toggl.Ultrawave.Exceptions;
+using Xunit;
+
+namespace Toggl.Ultrawave.Tests.Integration
+{
+    public abstract class AuthenticatedEndpointBaseTests<T>
+    {
+        protected abstract IObservable<T> CallingEndpointWith((string email, string password) credentials);
+
+        private Action callingEndpointWith((string email, string password) credentials)
+            => () => CallingEndpointWith(credentials).Wait();
+
+        [Fact]
+        public async Task WorksForExistingUser()
+        {
+            var credentials = await User.Create();
+
+            callingEndpointWith(credentials).ShouldNotThrow();
+        }
+
+        [Fact]
+        public void FailsForNonExistingUser()
+        {
+            var email = $"non-existing-email-{Guid.NewGuid()}@ironicmocks.toggl.com";
+            var wrongCredentials = (email, "123456789");
+
+            callingEndpointWith(wrongCredentials).ShouldThrow<ApiException>();
+
+            // TODO: check for error code
+        }
+
+        [Fact]
+        public async Task FailsIfUsingTheWrongPassword()
+        {
+            var (email, password) = await User.Create();
+            var wrongCredentials = (email, $"{password}1");
+
+            callingEndpointWith(wrongCredentials).ShouldThrow<ApiException>();
+
+            // TODO: check for error code
+        }
+    }
+}
--- Toggl.Ultrawave.Tests.Integration/UserClientTests.cs
+++ Toggl.Ultrawave.Tests.Integration/UserClientTests.cs
@@ -1,59 +1,19 @@
 ﻿﻿using System;
-using System.Reactive.Linq;
-using System.Threading.Tasks;
-using FluentAssertions;
+using Toggl.Multivac.Models;
 using Toggl.Ultrawave.Clients;
-using Toggl.Ultrawave.Exceptions;
-using Xunit;
 
 namespace Toggl.Ultrawave.Tests.Integration
 {
     public class UserClientTests
     {
-        public class TheGetMethod
+        public class TheGetMethod : AuthenticatedEndpointBaseTests<IUser>
         {
             private readonly IUserClient userClient = new TogglClient(ApiEnvironment.Staging).User;
 
-            [Fact]
-            public async Task WorksForExistingUsers()
-            {
-                var (email, password) = await User.Create();
+            protected override IObservable<IUser> CallingEndpointWith((string email, string password) credentials)
+                => userClient.Get(credentials.email, credentials.password);
 
-                Action tryingToLoginWithValidCredentials = 
-                    () => userClient.Get(email, password).Wait();
-
-                tryingToLoginWithValidCredentials
-                    .ShouldNotThrow<ApiException>();
-                
-                //TODO: Include check for returned data
-            }
-
-            [Fact]
-            public void FailsForNonExistingEmails()
-            {
-                var email = $"non-existing-email-{Guid.NewGuid()}@ironicmocks.toggl.com";
-
-                Action tryingToLoginWithNonExistingCredential = 
-                    () => userClient.Get(email, "123456789").Wait();
-
-                tryingToLoginWithNonExistingCredential
-                    .ShouldThrow<ApiException>();
-                
-                //TODO: Include check expected error message/code
-            }
-
-            [Fact]
-            public async Task FailsIfUsingTheWrongPassword()
-            {
-                var (email, password) = await User.Create();
-
-                Action tryingToLoginWithWrongPassword = 
-                    () => userClient.Get(email, "123457").Wait();
-
-                tryingToLoginWithWrongPassword
-                    .ShouldThrow<ApiException>();
-                //TODO: Include check expected error message/code
-            }
+            // TODO: check that expected data is returned
         }
     }
 }
