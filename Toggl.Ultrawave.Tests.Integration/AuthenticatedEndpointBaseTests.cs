a9c0e9a518f2292e9ceba9c1150fc3ebdb0ddd6e
Paul Scharf <amul@amulware.net>
2017-05-16 11:54:01 +0200

    :white_check_mark: Test authentication with api token (#132)
    * :white_check_mark: Test authentication with api token
    
    * :lipstick:

--- Toggl.Ultrawave.Tests.Integration/AuthenticatedEndpointBaseTests.cs
+++ Toggl.Ultrawave.Tests.Integration/AuthenticatedEndpointBaseTests.cs
@@ -16,11 +16,32 @@ namespace Toggl.Ultrawave.Tests.Integration
             => () => CallEndpointWith(togglClient).Wait();
 
         [Fact]
-        public async Task WorksForExistingUser()
+        public async Task WorksWithPassword()
         {
             var credentials = await User.Create();
 
             CallingEndpointWith(TogglClientWith(credentials)).ShouldNotThrow();
+        }
+
+        [Fact]
+        public async Task WorksWithApiToken()
+        {
+            var (_, user) = await SetupTestUser();
+            var apiTokenCredentials = Credentials.WithApiToken(user.ApiToken);
+
+            CallingEndpointWith(TogglClientWith(apiTokenCredentials)).ShouldNotThrow();
+        }
+
+        [Fact]
+        public async Task ReturnsTheSameWhetherUsingPasswordOrApiToken()
+        {
+            var (passwordClient, user) = await SetupTestUser();
+            var apiTokenClient = TogglClientWith(Credentials.WithApiToken(user.ApiToken));
+
+            var passwordReturn = await CallEndpointWith(passwordClient);
+            var apiTokenReturn = await CallEndpointWith(apiTokenClient);
+
+            passwordReturn.ShouldBeEquivalentTo(apiTokenReturn);
         }
 
         [Fact]
@@ -30,19 +51,30 @@ namespace Toggl.Ultrawave.Tests.Integration
             var wrongCredentials = Credentials.WithPassword(email, "123456789");
 
             CallingEndpointWith(TogglClientWith(wrongCredentials)).ShouldThrow<ApiException>();
-
-            // TODO: check for error code
         }
 
         [Fact]
-        public async Task FailsIfUsingTheWrongPassword()
+        public async Task FailsWithWrongPassword()
         {
             var (email, password) = await User.CreateEmailPassword();
             var wrongCredentials = Credentials.WithPassword(email, $"{password}1");
 
             CallingEndpointWith(TogglClientWith(wrongCredentials)).ShouldThrow<ApiException>();
+        }
 
-            // TODO: check for error code
+        [Fact]
+        public void FailsWithWrongApiToken()
+        {
+            var wrongApiToken = Guid.NewGuid().ToString("N");
+            var wrongApiTokenCredentials = Credentials.WithApiToken(wrongApiToken);
+
+            CallingEndpointWith(TogglClientWith(wrongApiTokenCredentials)).ShouldThrow<ApiException>();
+        }
+
+        [Fact]
+        public void FailsWithoutCredentials()
+        {
+            CallingEndpointWith(TogglClientWith(Credentials.None)).ShouldThrow<ApiException>();
         }
     }
 }
