0756fc907cff4adbf77b45a63a4b93e970bc0e26
William Barbosa <william.sb@hotmail.com>
2017-06-06 08:23:12 +0200

    :fire: Burn flaky tests (#170)

--- Toggl.Foundation.Tests/MvvmCross/ViewModels/LoginViewModelTests.cs
+++ Toggl.Foundation.Tests/MvvmCross/ViewModels/LoginViewModelTests.cs
@@ -1,17 +1,9 @@
-﻿﻿using System;
-using System.Reactive.Linq;
-using FluentAssertions;
-using NSubstitute;
+﻿using NSubstitute;
 using Toggl.Ultrawave;
 using Toggl.Foundation.MvvmCross.ViewModels;
 using Toggl.Multivac.Models;
-using Toggl.Ultrawave.Exceptions;
-using Xunit;
 using Microsoft.Reactive.Testing;
 using Toggl.Foundation.MvvmCross.Services;
-using Toggl.Ultrawave.Network;
-using Toggl.Foundation.DataSources;
-using Toggl.Multivac;
 
 namespace Toggl.Foundation.Tests.MvvmCross.ViewModels
 {
@@ -31,127 +23,6 @@ namespace Toggl.Foundation.Tests.MvvmCross.ViewModels
                 base.AdditionalSetup();
 
                 Ioc.RegisterSingleton(ApiFactory);
-            }
-        }
-
-        public class TheEmailIsValidProperty : LoginViewModelTest
-        {
-            [Fact]
-            public void ReturnsTrueIfTheUsernameIsValid()
-            {
-                ViewModel.Email = ValidEmail;
-
-                ViewModel.EmailIsValid.Should().BeTrue();
-            }
-
-            [Fact]
-            public void ReturnsFalseIfTheUsernameIsInvalid()
-            {
-                ViewModel.Email = InvalidEmail;
-
-                ViewModel.EmailIsValid.Should().BeFalse();
-            }
-        }
-
-        public class TheLoginCommand : LoginViewModelTest
-        {
-            public TheLoginCommand()
-            {
-                ViewModel.Email = ValidEmail;
-            }
-
-            [Fact]
-            public void CanNotBeExecutedWhileTheEmailIsInvalid()
-            {
-                ViewModel.Email = InvalidEmail;
-            
-                ViewModel.LoginCommand.CanExecute().Should().BeFalse();
-            }
-
-            [Fact]
-            public void CanNotBeExecutedAgainUntilTheObservableReturns()
-            {
-                DataSource.User
-                          .Login(Arg.Any<Email>(), Arg.Any<string>())
-                          .Returns(Observable.Return(User).Delay(TimeSpan.FromMinutes(100)));
-
-                ViewModel.LoginCommand.Execute();
-            
-                ViewModel.LoginCommand.CanExecute().Should().BeFalse();
-            }
-
-            [Fact]
-            public void CanBeExecutedAgainIfTheObservableReturns()
-            {
-                DataSource.User
-                          .Login(Arg.Any<Email>(), Arg.Any<string>())
-                          .Returns(Observable.Return(User)
-                                             .SubscribeOn(TestScheduler));
-
-                ViewModel.LoginCommand.Execute();
-                TestScheduler.Start();
-
-                ViewModel.LoginCommand.CanExecute().Should().BeTrue();
-            }
-
-            [Fact]
-            public void CanBeExecutedAgainIfTheObservableFails()
-            {
-                DataSource.User
-                    .Login(Arg.Any<Email>(), Arg.Any<string>())
-                    .Returns(Observable.Throw<IUser>(new ApiException(""))
-                        .SubscribeOn(TestScheduler));
-
-                ViewModel.LoginCommand.Execute();
-                TestScheduler.Start();
-
-                ViewModel.LoginCommand.CanExecute().Should().BeTrue();
-            }
-
-            [Fact]
-            public void CreatesANewApiWithTheReturnedUserToken()
-            {
-                var expectedHeader = Credentials.WithApiToken(User.ApiToken).Header;
-
-                DataSource.User
-                    .Login(Arg.Any<Email>(), Arg.Any<string>())
-                    .Returns(Observable.Return(User));
-
-                ViewModel.LoginCommand.Execute();
-
-                ApiFactory.Received()
-                          .CreateApiWith(Arg.Is<Credentials>(
-                              credentials => credentials.Header.Value == expectedHeader.Value)
-                          );
-            }
-
-            [Fact]
-            public void RegistersANewApiWithTheReturnedUserToken()
-            {
-                var oldApi = Ioc.Resolve<ITogglApi>();
-
-                DataSource.User
-                    .Login(Arg.Any<Email>(), Arg.Any<string>())
-                    .Returns(Observable.Return(User));
-
-                ViewModel.LoginCommand.Execute();
-
-                var actualApi = Ioc.Resolve<ITogglApi>();
-                actualApi.Should().NotBe(oldApi);
-            }
-
-            [Fact]
-            public void RegisterANewDataSourceWithTheReturnedUserToken()
-            {
-                var oldDataSource = Ioc.Resolve<ITogglDataSource>();
-                DataSource.User
-                    .Login(Arg.Any<Email>(), Arg.Any<string>())
-                    .Returns(Observable.Return(User));
-
-                ViewModel.LoginCommand.Execute();
-
-                var newDataSource = Ioc.Resolve<ITogglDataSource>();
-                newDataSource.Should().NotBe(oldDataSource);
             }
         }
     }
