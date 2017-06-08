668c612d843ff65f36d68589b68f0b70f92b671c
Paul Scharf <amul@amulware.net>
2017-06-08 16:16:02 +0200

    :racehorse: Prevent unnecessary allocations in user agent (#193)

--- Toggl.Ultrawave/Network/UserAgent.cs
+++ Toggl.Ultrawave/Network/UserAgent.cs
@@ -4,18 +4,16 @@ namespace Toggl.Ultrawave.Network
 {
     public sealed class UserAgent
     {
-        private readonly string version;
-        private readonly string agentName;
+        private readonly string userAgentString;
 
         public UserAgent(string agentName, string version)
         {
             Ensure.Argument.IsNotNullOrWhiteSpaceString(version, nameof(version));
             Ensure.Argument.IsNotNullOrWhiteSpaceString(agentName, nameof(agentName));
 
-            this.version = version;
-            this.agentName = agentName;
+            userAgentString = $"{agentName}/{version}";
         }
 
-        public override string ToString() => $"{agentName}/{version}";
+        public override string ToString() => userAgentString;
     }
 }
