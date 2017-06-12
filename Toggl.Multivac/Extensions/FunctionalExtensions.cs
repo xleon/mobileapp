445f994405b4b5097c7191696723b7f2a1c759ee
Artūrs Eimanis <arturseimanis@gmail.com>
2017-06-12 19:44:30 +0300

    :construction: Add ForEach extension (#197)

--- Toggl.Multivac/Extensions/FunctionalExtensions.cs
+++ Toggl.Multivac/Extensions/FunctionalExtensions.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Toggl.Multivac.Extensions
 {
     public static class FunctionalExtensions
     {
         public static TResult Apply<T, TResult>(this T self, Func<T, TResult> funcToApply)
             => funcToApply(self);
+
+        public static void ForEach<T>(this IEnumerable<T> self, Action<T> action)
+        {
+            Ensure.Argument.IsNotNull(self, nameof(self));
+            Ensure.Argument.IsNotNull(action, nameof(action));
+
+            foreach (T item in self)
+            {
+                action(item);
+            }
+        }
     }
 }
