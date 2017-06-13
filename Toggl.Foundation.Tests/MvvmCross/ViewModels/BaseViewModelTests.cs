43e76a354dd01f2ae89720d3141de7089f508678
William Barbosa <william.sb@hotmail.com>
2017-06-13 22:32:29 +0200

    :bug: Fix Race condition in MvvmCross tests (#205)
    * :bug: Fix Race condition in MvvmCross tests
    
    * :bug: Move initialization to static constructor

--- Toggl.Foundation.Tests/MvvmCross/ViewModels/BaseViewModelTests.cs
+++ Toggl.Foundation.Tests/MvvmCross/ViewModels/BaseViewModelTests.cs
@@ -1,4 +1,9 @@
-﻿using MvvmCross.Test.Core;
+﻿using MvvmCross.Core;
+using MvvmCross.Core.Platform;
+using MvvmCross.Platform.Core;
+using MvvmCross.Platform.IoC;
+using MvvmCross.Platform.Platform;
+using MvvmCross.Test.Core;
 using NSubstitute;
 using Toggl.Foundation.DataSources;
 using Toggl.Foundation.MvvmCross.ViewModels;
@@ -7,9 +12,28 @@ using Toggl.Ultrawave;
 
 namespace Toggl.Foundation.Tests.MvvmCross.ViewModels
 {
-    public abstract class BaseViewModelTests<TViewModel> : MvxIoCSupportingTest
+    public abstract class BaseViewModelTests
+    {
+        static BaseViewModelTests()
+        {
+            if (MvxSingleton<IMvxIoCProvider>.Instance != null) return;
+
+            MvxSingletonCache.Initialize();
+            MvxSimpleIoCContainer.Initialize();
+
+            MvxSingleton<IMvxIoCProvider>.Instance.RegisterSingleton(MvxSingleton<IMvxIoCProvider>.Instance);
+            MvxSingleton<IMvxIoCProvider>.Instance.RegisterSingleton<IMvxTrace>(new TestTrace());
+            MvxSingleton<IMvxIoCProvider>.Instance.RegisterSingleton<IMvxSettings>(new MvxSettings());
+
+            MvxTrace.Initialize();
+        }
+    }
+
+    public abstract class BaseViewModelTests<TViewModel> : BaseViewModelTests
         where TViewModel : BaseViewModel
     {
+        protected IMvxIoCProvider Ioc { get; private set; }
+
         protected BaseViewModelTests()
         {
             Setup();
@@ -23,11 +47,18 @@ namespace Toggl.Foundation.Tests.MvvmCross.ViewModels
         protected ITogglDatabase Database { get; } = Substitute.For<ITogglDatabase>();
         protected ITogglDataSource DataSource { get; } = Substitute.For<ITogglDataSource>();
 
-        protected override void AdditionalSetup()
+        private void Setup()
+        {
+            Ioc = MvxSingleton<IMvxIoCProvider>.Instance;
+
+            AdditionalSetup();
+        }
+
+        protected virtual void AdditionalSetup()
         {
             Ioc.RegisterSingleton(Api);
             Ioc.RegisterSingleton(Database);
             Ioc.RegisterSingleton(DataSource);
         }
-     }
+    }
 }
