e12ec5e495236d324ecc3f43e7744ab113ea1461
William Barbosa <william.sb@hotmail.com>
2017-06-14 10:40:07 +0200

    :package: Add Color and Visibility Plugins (#204)

---
+++ Toggl.Daneel/Bootstrap/ColorPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace Toggl.Daneel.Bootstrap
+{
+    public class ColorPluginBootstrap
+        : MvxLoaderPluginBootstrapAction<MvvmCross.Plugins.Color.PluginLoader, MvvmCross.Plugins.Color.iOS.Plugin>
+    {
+    }
+}
---
+++ Toggl.Daneel/Bootstrap/VisibilityPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace Toggl.Daneel.Bootstrap
+{
+    public class VisibilityPluginBootstrap
+        : MvxLoaderPluginBootstrapAction<MvvmCross.Plugins.Visibility.PluginLoader, MvvmCross.Plugins.Visibility.iOS.Plugin>
+    {
+    }
+}
