1b2e373a551c4af8cd57382e043d04816d5b8e0c
William Barbosa <william.sb@hotmail.com>
2017-06-15 13:54:09 +0200

    :construction: Common animation code (#210)
    * :construction: Add common curves
    
    * :construction: Allow Daneel to consume common curves
    
    * :construction: Add common Timings
    
    * :fire:

---
+++ Toggl.Daneel/Extensions/AnimationExtensions.cs
@@ -0,0 +1,11 @@
+﻿using CoreAnimation;
+using static Toggl.Foundation.MvvmCross.Helper.Animation;
+
+namespace Toggl.Daneel.Extensions
+{
+    public static class AnimationExtensions
+    {
+        public static CAMediaTimingFunction ToMediaTimingFunction(this CubicBezierCurve self)
+            => new CAMediaTimingFunction(self.P0, self.P1, self.P2, self.P3);
+    }
+}
