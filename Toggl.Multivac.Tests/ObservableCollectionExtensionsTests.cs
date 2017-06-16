a25f66f21bcca99dcdea201feb8cb425132bc4f8
Artūrs Eimanis <arturseimanis@gmail.com>
2017-06-16 15:44:36 +0300

    Add ObservableCollection.AddRange() extension (#212)
    * :construction: Add Collection.AddRange() extension
    
    * :white_check_mark: Add CollectionExtensionsTests

---
+++ Toggl.Multivac.Tests/ObservableCollectionExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using FluentAssertions;
+using Xunit;
+
+namespace Toggl.Multivac.Tests
+{
+    public class ObservableCollectionExtensionsTests
+    {
+        public class TheAddRangeMethod
+        {
+            [Fact]
+            public void AddsAllItems()
+            {
+                int[] initialItems = { 1, 2 };
+                int[] newItems = { 3, 4, 5 };
+                var collection = new ObservableCollection<int> (initialItems);
+
+                collection.AddRange(newItems);
+
+                collection.Should().HaveCount(initialItems.Length + newItems.Length)
+                          .And.Contain(initialItems)
+                          .And.Contain(newItems);
+            }
+        }
+    }
+}
