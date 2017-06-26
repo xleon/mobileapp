248fb9b143e3490805893ce074e0e608467feb85
Artūrs Eimanis <arturseimanis@gmail.com>
2017-06-26 13:40:07 +0300

    :construction: Add time entries viewmodel (#226)
    * :construction: Add TimeEntriesViewModel
    
    * :white_check_mark: Add TimeEntriesViewModelTests

---
+++ Toggl.Foundation.MvvmCross/ViewModels/TimeEntriesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Toggl.Multivac.Extensions;
+using Toggl.Multivac.Models;
+
+namespace Toggl.Foundation.MvvmCross.ViewModels
+{
+    public class TimeEntriesViewModel : BaseViewModel
+    {
+        public ObservableCollection<ITimeEntry> TimeEntries { get; }
+            = new ObservableCollection<ITimeEntry>();
+
+        public async override Task Initialize()
+        {
+            await base.Initialize();
+            DataSource.TimeEntries.GetAll().Subscribe(timeEntries => timeEntries.ForEach(TimeEntries.Add));
+        }
+    }
+}
---
+++ Toggl.Foundation.Tests/MvvmCross/ViewModels/TimeEntriesViewModelTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NSubstitute;
+using Toggl.Foundation.MvvmCross.ViewModels;
+using Toggl.Multivac.Models;
+using Xunit;
+using TimeEntry = Toggl.Ultrawave.Models.TimeEntry;
+
+namespace Toggl.Foundation.Tests.MvvmCross.ViewModels
+{
+    public class TimeEntriesViewModelTests
+    {
+        public class TheTimeEntriesProperty : BaseViewModelTests<TimeEntriesViewModel>
+        {
+            [Fact]
+            public async Task ReturnsAllTimeEntries()
+            {
+                var timeEntries = new List<ITimeEntry>()
+                {
+                    new TimeEntry { Description = "entry1" },
+                    new TimeEntry { Description = "entry2" },
+                    new TimeEntry { Description = "entry3" }
+                };
+                var observable = Observable.Return(timeEntries);
+                DataSource.TimeEntries.GetAll().Returns(observable);
+
+                await ViewModel.Initialize();
+
+                ViewModel.TimeEntries.Should().HaveCount(timeEntries.Count);
+                ViewModel.TimeEntries.ShouldBeEquivalentTo(timeEntries);
+            }
+        }
+    }
+}
