1313f3c80419794dcc4a30d185470d56ba28b213
William Barbosa <william.sb@hotmail.com>
2017-07-04 10:40:00 -0300

    :art: TimeEntryViewCell (#252)
    * :art: TimeEntryViewCell
    
    * :green_heart:
    
    * :lipstick:

---
+++ Toggl.Daneel/Views/TimeEntryViewCell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace Toggl.Daneel.Views
+{
+    public partial class TimeEntryViewCell : UITableViewCell
+    {
+        public static readonly NSString Key = new NSString(nameof(TimeEntryViewCell));
+        public static readonly UINib Nib;
+
+        static TimeEntryViewCell()
+        {
+            Nib = UINib.FromName(nameof(TimeEntryViewCell), NSBundle.MainBundle);
+        }
+
+        protected TimeEntryViewCell(IntPtr handle) 
+            : base(handle)
+        {
+        }
+    }
+}
---
+++ Toggl.Daneel/Views/TimeEntryViewCell.designer.cs
@@ -0,0 +1,64 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace Toggl.Daneel.Views
+{
+    [Register ("TimeEntryViewCell")]
+    partial class TimeEntryViewCell
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ContinueButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel DescriptionLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel ProjectLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel TaskLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel TimeLabel { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (ContinueButton != null) {
+                ContinueButton.Dispose ();
+                ContinueButton = null;
+            }
+
+            if (DescriptionLabel != null) {
+                DescriptionLabel.Dispose ();
+                DescriptionLabel = null;
+            }
+
+            if (ProjectLabel != null) {
+                ProjectLabel.Dispose ();
+                ProjectLabel = null;
+            }
+
+            if (TaskLabel != null) {
+                TaskLabel.Dispose ();
+                TaskLabel = null;
+            }
+
+            if (TimeLabel != null) {
+                TimeLabel.Dispose ();
+                TimeLabel = null;
+            }
+        }
+    }
+}
