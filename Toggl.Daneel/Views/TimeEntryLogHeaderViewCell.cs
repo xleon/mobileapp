91ff259406b0be31302015284e7096d26b87d255
Artūrs Eimanis <arturseimanis@gmail.com>
2017-07-05 09:33:32 +0300

    :art: Add time entry log header view cell (#257)

---
+++ Toggl.Daneel/Views/TimeEntryLogHeaderViewCell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace Toggl.Daneel.Views
+{
+    public partial class TimeEntryLogHeaderViewCell : UITableViewHeaderFooterView
+    {
+        public static readonly NSString Key = new NSString(nameof(TimeEntryLogHeaderViewCell));
+        public static readonly UINib Nib;
+
+        static TimeEntryLogHeaderViewCell()
+        {
+            Nib = UINib.FromName(nameof(TimeEntryLogHeaderViewCell), NSBundle.MainBundle);
+        }
+
+        protected TimeEntryLogHeaderViewCell(IntPtr handle)
+            : base(handle)
+        {
+        }
+    }
+}
