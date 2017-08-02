3b3ad46fd02c26bb82566a921a008906dcf51a42
William Barbosa <william.sb@hotmail.com>
2017-08-02 11:13:54 -0300

    :construction_worker: Add TimeEntry builder (#330)
    * :construction_worker: Add TimeEntry builder
    
    * :lock: Make fields readonly again
    
    * :lipstick: Better error
    
    * :construction: Add Create method instead of initially setting date
    
    * :muscle: Improve validity check
    
    * :green_heart:
    
    * :construction: Fix description check
    
    * :chart_with_upwards_trend: Improve the stop method
    
    * :fire:

---
+++ Toggl.Foundation/Models/TimeEntry.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Toggl.Foundation.Models
+{
+    internal partial class TimeEntry
+    {
+        internal sealed class Builder
+        {
+            private const string ErrorMessage = "You need to set the {0} before building a time entry";
+
+            public static Builder Create() => new Builder();
+
+            public bool IsDirty { get; private set; }
+
+            public bool Billable { get; private set; }
+
+            public string Description { get; private set; }
+            
+            public DateTimeOffset Start { get; private set; }
+
+            private Builder() { }
+
+            public TimeEntry Build() 
+            {
+                ensureValidity();
+                return new TimeEntry(this);
+            }
+
+            public Builder SetStartDate(DateTimeOffset start)
+            {
+                Start = start;
+                return this;
+            }
+
+            public Builder SetIsDirty(bool isDirty)
+            {
+                IsDirty = isDirty;
+                return this;
+            }
+
+            public Builder SetDescription(string description)
+            {
+                Description = description;
+                return this;
+            }
+
+            public Builder SetBillable(bool billable)
+            {
+                Billable = billable;
+                return this;
+            }
+
+            private void ensureValidity()
+            {
+                if (Start == default(DateTimeOffset))
+                    throw new InvalidOperationException(string.Format(ErrorMessage, "start date"));
+
+                if (Description == null)
+                    throw new InvalidOperationException(string.Format(ErrorMessage, "description"));
+            }
+        }
+
+        private TimeEntry(Builder builder)
+        {
+            Start = builder.Start;
+            IsDirty = builder.IsDirty;
+            Billable = builder.Billable;
+            Description = builder.Description;
+        }
+    }
+}
