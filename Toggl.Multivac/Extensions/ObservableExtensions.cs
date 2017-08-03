34153bdf06cdd87b56a7bf0490b0dedc05b2e97e
Artūrs Eimanis <arturseimanis@gmail.com>
2017-08-03 11:29:22 +0300

    :construction: Add IObservable.Subscribe(onError, onCompleted) extension (#335)

---
+++ Toggl.Multivac/Extensions/ObservableExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Toggl.Multivac.Extensions
+{
+    public static class ObservableExtensions
+    {
+        private class Observer<T> : IObserver<T>
+        {
+            private readonly Action<Exception> onError;
+            private readonly Action onCompleted;
+
+            public Observer(Action<Exception> onError, Action onCompleted)
+            {
+                this.onError = onError;
+                this.onCompleted = onCompleted;
+            }
+
+            public void OnCompleted()
+                => onCompleted();
+
+            public void OnError(Exception error)
+                => onError(error);
+
+            public void OnNext(T value) {}
+        }
+
+        public static IDisposable Subscribe<T>(this IObservable<T> observable, Action<Exception> onError, Action onCompleted)
+        {
+            var observer = new Observer<T>(onError, onCompleted);
+            return observable.Subscribe(observer);
+        }
+    }
+}
