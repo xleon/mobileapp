e2104cc0f9c963df983b2cadc942fd480c04c3d5
William Barbosa <william.sb@hotmail.com>
2017-08-04 09:19:24 -0300

    :bug: Fix TimeEntry realm table (#346)
    * :bug: Fix TimeEntry realm table
    
    * :fire:

--- Toggl.PrimeRadiant.Realm/Models/RealmTimeEntry.cs
+++ Toggl.PrimeRadiant.Realm/Models/RealmTimeEntry.cs
@@ -7,34 +7,34 @@ namespace Toggl.PrimeRadiant.Realm
 {
     internal partial class RealmTimeEntry : RealmObject, IDatabaseTimeEntry
     {
-        public int WorkspaceId { get; }
+        public int WorkspaceId { get; set; }
 
-        public int? ProjectId { get; }
+        public int? ProjectId { get; set; }
 
-        public int? TaskId { get; }
+        public int? TaskId { get; set; }
 
-        public bool Billable { get; }
+        public bool Billable { get; set; }
 
-        public DateTimeOffset Start { get; }
+        public DateTimeOffset Start { get; set; }
 
-        public DateTimeOffset? Stop { get; }
+        public DateTimeOffset? Stop { get; set; }
 
-        public int Duration { get; }
+        public int Duration { get; set; }
 
-        public string Description { get; }
+        public string Description { get; set; }
 
-        public IList<string> Tags { get; }
+        public IList<string> Tags { get; set; }
 
-        public IList<int> TagIds { get; }
+        public IList<int> TagIds { get; set; }
 
-        public DateTimeOffset At { get; }
+        public DateTimeOffset At { get; set; }
 
-        public DateTimeOffset? ServerDeletedAt { get; }
+        public DateTimeOffset? ServerDeletedAt { get; set; }
 
-        public int UserId { get; }
+        public int UserId { get; set; }
 
-        public string CreatedWith { get; }
+        public string CreatedWith { get; set; }
 
-        public bool IsDeleted { get; }
+        public bool IsDeleted { get; set; }
     }
 }
