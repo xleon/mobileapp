58ee792d39b71e843cfc8cc3fb12d0dfd368fffc
Simon Rozsival <simon@rozsival.com>
2017-08-07 16:49:00 +0300

    :white_check_mark: Add unit tests for API exceptions
    * :white_check_mark: Add tests for API exceptions
    
    * :lipstick: :white_check_mark: Shorten the assertions
    
    * :construction: :white_check_mark: Use MemberData for theories
    
    * :lipstick: Use expression bodied function
    
    * :construction: :white_check_mark: Use a declarative way of generating a list unknown HTTP status codes

---
+++ Toggl.Ultrawave.Tests/Exceptions/ApiErrorResponsesTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using FluentAssertions;
+using Toggl.Ultrawave.Exceptions;
+using Toggl.Ultrawave.Helpers;
+using Toggl.Ultrawave.Network;
+using Xunit;
+using static System.Net.HttpStatusCode;
+using NotImplementedException = Toggl.Ultrawave.Exceptions.NotImplementedException;
+
+namespace Toggl.Ultrawave.Tests.Exceptions
+{
+    public class ApiErrorResponsesTests
+    {
+        public class ClientErrors
+        {
+            [Theory]
+            [MemberData(nameof(ClientErrorsList), MemberType = typeof(ApiErrorResponsesTests))]
+            public void ReturnsClientErrorException(HttpStatusCode httpStatusCode, Type expectedExceptionType)
+            {
+                var response = createErrorResponse(httpStatusCode);
+
+                var exception = ApiExceptions.ForResponse(response);
+
+                exception.Should().BeAssignableTo<ClientErrorException>().And.BeOfType(expectedExceptionType);
+            }
+        }
+        
+        public class ServerErrors
+        {
+            [Theory]
+            [MemberData(nameof(ServerErrorsList), MemberType = typeof(ApiErrorResponsesTests))]
+            public void ReturnsServerErrorException(HttpStatusCode httpStatusCode, Type expectedExceptionType)
+            {
+                var response = createErrorResponse(httpStatusCode);
+
+                var exception = ApiExceptions.ForResponse(response);
+
+                exception.Should().BeAssignableTo<ServerErrorException>().And.BeOfType(expectedExceptionType);
+            }
+        }
+
+        public class UnknownErrors
+        {
+            [Theory]
+            [MemberData(nameof(UnknownErrorsList), MemberType = typeof(ApiErrorResponsesTests))]
+            public void ReturnsUnknownApiError(HttpStatusCode httpStatusCode)
+            {
+                var response = createErrorResponse(httpStatusCode);
+
+                var exception = ApiExceptions.ForResponse(response);
+
+                exception.Should().BeOfType<UnknownApiErrorException>()
+                    .Which.HttpCode.Should().Equals(httpStatusCode);
+            }
+        }
+
+        private static Response createErrorResponse(HttpStatusCode code, string rawData = "")
+            => new Response(rawData, false, "application/json", code);
+
+        public static IEnumerable<object[]> ClientErrorsList
+            => new[]
+            {
+                new object[] { BadRequest, typeof(BadRequestException) },
+                new object[] { Unauthorized, typeof(UnauthorizedException) },
+                new object[] { PaymentRequired, typeof(PaymentRequiredException) },
+                new object[] { Forbidden, typeof(ForbiddenException) },
+                new object[] { NotFound, typeof(NotFoundException) },
+                new object[] { Gone, typeof(ApiDeprecatedException) },
+                new object[] { RequestEntityTooLarge, typeof(RequestEntityTooLargeException) },
+                new object[] { 418, typeof(ClientDeprecatedException) }, // HTTP 418 - I Am a Teapot
+                new object[] { 429, typeof(TooManyRequestsException) } // HTTP 429 - Too Many Requests
+            };
+
+        public static IEnumerable<object[]> ServerErrorsList
+            => new[]
+            {
+                new object[] { InternalServerError, typeof(InternalServerErrorException) },
+                new object[] { NotImplemented, typeof(NotImplementedException) },
+                new object[] { BadGateway, typeof(BadGatewayException) },
+                new object[] { ServiceUnavailable, typeof(ServiceUnavailableException) },
+                new object[] { GatewayTimeout, typeof(GatewayTimeoutException) },
+                new object[] { HttpVersionNotSupported, typeof(HttpVersionNotSupportedException) }
+            };
+
+        private static IEnumerable<object[]> KnownErrorsList
+            => ClientErrorsList.Concat(ServerErrorsList);
+
+        private static bool IsKnownError(int code)
+            => KnownErrorsList.Any(item => (int)item[0] == code);
+
+        public static IEnumerable<object[]> UnknownErrorsList
+            => Enumerable.Range(400, 200)
+                .Where(code => !IsKnownError(code))
+                .Select(code => new object[] { (HttpStatusCode) code });
+    }
+}
