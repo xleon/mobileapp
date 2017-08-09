0323654c832e9c7aa99cd7d17a831b44218e66e8
Artūrs Eimanis <arturseimanis@gmail.com>
2017-08-09 17:16:04 +0300

    :art: Add EditTimeEntryViewController (#361)

---
+++ Toggl.Daneel/ViewControllers/EditTimeEntryViewController.cs
@@ -0,0 +1,24 @@
+﻿using CoreGraphics;
+using MvvmCross.iOS.Views;
+using Toggl.Daneel.Presentation.Attributes;
+
+namespace Toggl.Daneel.ViewControllers
+{
+    [ModalCardPresentation]
+    public partial class EditTimeEntryViewController : MvxViewController
+    {
+        private const int switchHeight = 24;
+
+        public EditTimeEntryViewController() : base(nameof(EditTimeEntryViewController), null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var scale = switchHeight / BillableSwitch.Frame.Height;
+            BillableSwitch.Transform = CGAffineTransform.MakeScale(scale, scale);
+        }
+    }
+}
---
+++ Toggl.Daneel/ViewControllers/EditTimeEntryViewController.designer.cs
@@ -0,0 +1,130 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace Toggl.Daneel.ViewControllers
+{
+	[Register ("EditViewController")]
+	partial class EditTimeEntryViewController
+	{
+		[Outlet]
+		UIKit.UIStackView AddDescriptionView { get; set; }
+
+		[Outlet]
+		UIKit.UIStackView AddProjectAndTaskView { get; set; }
+
+		[Outlet]
+		UIKit.UIStackView AddTagsView { get; set; }
+
+		[Outlet]
+		UIKit.UISwitch BillableSwitch { get; set; }
+
+		[Outlet]
+		UIKit.UIButton CloseButton { get; set; }
+
+		[Outlet]
+		UIKit.UIButton ConfirmButton { get; set; }
+
+		[Outlet]
+		UIKit.UIButton DeleteButton { get; set; }
+
+		[Outlet]
+		UIKit.UILabel DescriptionLabel { get; set; }
+
+		[Outlet]
+		UIKit.UILabel DurationLabel { get; set; }
+
+		[Outlet]
+		UIKit.UIView ProjectDot { get; set; }
+
+		[Outlet]
+		UIKit.UILabel ProjectLabel { get; set; }
+
+		[Outlet]
+		UIKit.UILabel StartDateLabel { get; set; }
+
+		[Outlet]
+		UIKit.UILabel StartTimeLabel { get; set; }
+
+		[Outlet]
+		UIKit.UILabel TaskLabel { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (AddDescriptionView != null) {
+				AddDescriptionView.Dispose ();
+				AddDescriptionView = null;
+			}
+
+			if (AddProjectAndTaskView != null) {
+				AddProjectAndTaskView.Dispose ();
+				AddProjectAndTaskView = null;
+			}
+
+			if (AddTagsView != null) {
+				AddTagsView.Dispose ();
+				AddTagsView = null;
+			}
+
+			if (BillableSwitch != null) {
+				BillableSwitch.Dispose ();
+				BillableSwitch = null;
+			}
+
+			if (CloseButton != null) {
+				CloseButton.Dispose ();
+				CloseButton = null;
+			}
+
+			if (ConfirmButton != null) {
+				ConfirmButton.Dispose ();
+				ConfirmButton = null;
+			}
+
+			if (DeleteButton != null) {
+				DeleteButton.Dispose ();
+				DeleteButton = null;
+			}
+
+			if (DescriptionLabel != null) {
+				DescriptionLabel.Dispose ();
+				DescriptionLabel = null;
+			}
+
+			if (DurationLabel != null) {
+				DurationLabel.Dispose ();
+				DurationLabel = null;
+			}
+
+			if (ProjectDot != null) {
+				ProjectDot.Dispose ();
+				ProjectDot = null;
+			}
+
+			if (ProjectLabel != null) {
+				ProjectLabel.Dispose ();
+				ProjectLabel = null;
+			}
+
+			if (StartDateLabel != null) {
+				StartDateLabel.Dispose ();
+				StartDateLabel = null;
+			}
+
+			if (StartTimeLabel != null) {
+				StartTimeLabel.Dispose ();
+				StartTimeLabel = null;
+			}
+
+			if (TaskLabel != null) {
+				TaskLabel.Dispose ();
+				TaskLabel = null;
+			}
+		}
+	}
+}
