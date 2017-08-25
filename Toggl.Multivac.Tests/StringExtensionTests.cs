37fb41935f03026b995f3b1bbb0ea7e1cbbe98e1
William Barbosa <william.sb@hotmail.com>
2017-08-25 08:47:38 -0300

    :construction: Add ContainsIgnoringCase extension (#459)
    * :construction: Add ContainsIgnoringCase extension
    
    * :package: Add FsCheck
    
    * :white_check_mark: Add extension tests
    
    * :fire:

---
+++ Toggl.Multivac.Tests/StringExtensionTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using FluentAssertions;
+using FsCheck;
+using FsCheck.Xunit;
+using Toggl.Multivac.Extensions;
+
+namespace Toggl.Multivac.Tests
+{
+    public sealed class StringExtensionTests
+    {
+        public sealed class TheContainsIgnoringCaseMethod
+        {
+            [Property]
+            public void MatchesSubstringsEvenIfTheyAreAllSearchedInAllCaps(string testString)
+            {
+                if (string.IsNullOrEmpty(testString)) return;
+
+                var length = testString.Length;
+                var startIndex = Gen.Choose(0, length - 1).Sample(1, 1).Single();
+                var substring = testString.Substring(startIndex, length - startIndex);
+
+                testString.ContainsIgnoringCase(substring.ToUpper()).Should().BeTrue();
+            }
+
+            [Property]
+            public void MatchesSubstringsEvenIfTheyAreAllSearchedInAllLowerCase(string testString)
+            {
+                if (string.IsNullOrEmpty(testString)) return;
+
+                var length = testString.Length;
+                var startIndex = Gen.Choose(0, length - 1).Sample(1, 1).Single();
+                var substring = testString.Substring(startIndex, length - startIndex);
+
+                testString.ContainsIgnoringCase(substring.ToLower()).Should().BeTrue();
+            }
+        }
+    }
+}
---
+++ Toggl.Multivac/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Toggl.Multivac.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool ContainsIgnoringCase(this string self, string value)
+            => self.IndexOf(value, StringComparison.OrdinalIgnoreCase) >= 0;
+    }
+}
