6077a06c16f38542db54667635a20f563dba7014
Simon Rozsival <simon@rozsival.com>
2017-08-31 18:02:51 +0200

    :sparkles: Persist time entries after fetch - state machine state (#484)
    * :sparkles: Add basic state for persisting time entries
    
    * :white_check_mark: Add tests

---
+++ Toggl.Foundation.Tests/Sync/States/PersistTimeEntriesStateTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using NSubstitute;
+using Toggl.Foundation.Models;
+using Toggl.Foundation.Sync;
+using Toggl.Foundation.Sync.States;
+using Toggl.Multivac.Models;
+using Toggl.PrimeRadiant;
+using Toggl.PrimeRadiant.Models;
+using TimeEntry = Toggl.Ultrawave.Models.TimeEntry;
+
+namespace Toggl.Foundation.Tests.Sync.States
+{
+    public sealed class PersistTimeEntriesStateTests : PersistStateTests
+    {
+        public PersistTimeEntriesStateTests()
+            : base(new TheStartMethod())
+        {
+        }
+
+        private sealed class TheStartMethod
+            : TheStartMethod<PersistTimeEntriesState, ITimeEntry, IDatabaseTimeEntry>
+        {
+            protected override PersistTimeEntriesState CreateState(ITogglDatabase database)
+                => new PersistTimeEntriesState(database);
+
+            protected override List<ITimeEntry> CreateEmptyList() => new List<ITimeEntry>();
+
+            protected override List<ITimeEntry> CreateListWithOneItem(DateTimeOffset? at = null)
+                => new List<ITimeEntry> { new TimeEntry { At = at ?? DateTimeOffset.Now, Description = Guid.NewGuid().ToString() } };
+
+            protected override FetchObservables CreateObservablesWhichFetchesTwice()
+                => CreateFetchObservables(
+                    null, new SinceParameters(null),
+                    timeEntries: Observable.Create<List<ITimeEntry>>(observer =>
+                    {
+                        observer.OnNext(CreateEmptyList());
+                        observer.OnNext(CreateEmptyList());
+                        return () => { };
+                    }));
+
+            protected override bool OtherSinceDatesDidntChange(ISinceParameters old, ISinceParameters next, DateTimeOffset at)
+                => next.Workspaces == old.Workspaces
+                   && next.Projects == old.Projects
+                   && next.Clients == old.Clients
+                   && next.Tags == old.Tags
+                   && next.Tasks == old.Tasks
+                   && next.TimeEntries == at;
+
+            protected override Transition<FetchObservables> CreateTransition(
+                ISinceParameters since = null,
+                List<ITimeEntry> timeEntries = null)
+            {
+                var fetchObservables = new FetchObservables(
+                    since ?? new SinceParameters(null),
+                    Observable.Return(new List<IWorkspace>()),
+                    Observable.Return(new List<IClient>()),
+                    Observable.Return(new List<IProject>()),
+                    Observable.Return(timeEntries ?? new List<ITimeEntry>()));
+                var fetchTransition = new Transition<FetchObservables>(new StateResult<FetchObservables>(), fetchObservables);
+
+                return fetchTransition;
+            }
+
+            protected override List<ITimeEntry> CreateComplexListWhereTheLastUpdateEntityIsDeleted(DateTimeOffset? at)
+                => createComplexList(at ?? DateTimeOffset.Now);
+
+            private List<ITimeEntry> createComplexList(DateTimeOffset at)
+                => new List<ITimeEntry>
+                {
+                    new TimeEntry { At = at.AddDays(-1), Description = Guid.NewGuid().ToString() },
+                    new TimeEntry { At = at.AddDays(-3), Description = Guid.NewGuid().ToString() },
+                    new TimeEntry { At = at, ServerDeletedAt = at, Description = Guid.NewGuid().ToString() },
+                    new TimeEntry { At = at.AddDays(-2), Description = Guid.NewGuid().ToString() }
+                };
+
+            protected override void SetupDatabaseBatchUpdateMocksToReturnDatabaseEntitiesAndFilterOutDeletedEntities(ITogglDatabase database, List<ITimeEntry> timeEntries = null)
+            {
+                var foundationTimeEntries = timeEntries?.Where(timeEntry => timeEntry.ServerDeletedAt == null).Select(Models.TimeEntry.Clean);
+                database.TimeEntries.BatchUpdate(null, null)
+                    .ReturnsForAnyArgs(Observable.Return(foundationTimeEntries));
+            }
+
+            protected override void SetupDatabaseBatchUpdateToThrow(ITogglDatabase database, Func<Exception> exceptionFactory)
+                => database.TimeEntries.BatchUpdate(null, null).ReturnsForAnyArgs(_ => throw exceptionFactory());
+
+            protected override void AssertBatchUpdateWasCalled(ITogglDatabase database, List<ITimeEntry> timeEntries = null)
+            {
+                database.TimeEntries.Received().BatchUpdate(Arg.Is<IEnumerable<IDatabaseTimeEntry>>(
+                        list => list.Count() == timeEntries.Count && list.All(shouldBePersistedAndIsClean(timeEntries))),
+                    Arg.Any<Func<IDatabaseTimeEntry, IDatabaseTimeEntry, ConflictResolutionMode>>());
+            }
+
+            private Func<IDatabaseTimeEntry, bool> shouldBePersistedAndIsClean(List<ITimeEntry> timeEntries)
+                => persisted => persisted.IsDirty == false && timeEntries.Any(te => te.Description == persisted.Description);
+        }
+    }
+}
---
+++ Toggl.Foundation/Sync/States/PersistTimeEntriesState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Toggl.Foundation.Models;
+using Toggl.Foundation.Sync.ConflictResolution;
+using Toggl.Multivac.Models;
+using Toggl.PrimeRadiant;
+using Toggl.PrimeRadiant.Models;
+
+namespace Toggl.Foundation.Sync.States
+{
+    class PersistTimeEntriesState : BasePersistState<ITimeEntry, IDatabaseTimeEntry>
+    {
+        public PersistTimeEntriesState(ITogglDatabase database)
+            : base(database)
+        {
+        }
+
+        protected override IObservable<IEnumerable<ITimeEntry>> FetchObservable(FetchObservables fetch)
+            => fetch.TimeEntries;
+
+        protected override IDatabaseTimeEntry ConvertToDatabaseEntity(ITimeEntry entity)
+            => TimeEntry.Clean(entity);
+
+        protected override IObservable<IEnumerable<IDatabaseTimeEntry>> BatchUpdate(ITogglDatabase database, IEnumerable<IDatabaseTimeEntry> entities)
+            => database.TimeEntries.BatchUpdate(entities, Resolver.ForTimeEntries().Resolve);
+
+        protected override DateTimeOffset? LastUpdated(ISinceParameters old, IEnumerable<IDatabaseTimeEntry> entities)
+            => entities.Select(p => p?.At).Where(d => d.HasValue).DefaultIfEmpty(old.TimeEntries).Max();
+
+        protected override ISinceParameters UpdateSinceParameters(ISinceParameters old, DateTimeOffset? lastUpdated)
+            => new SinceParameters(old, timeEntries: lastUpdated);
+    }
+}
