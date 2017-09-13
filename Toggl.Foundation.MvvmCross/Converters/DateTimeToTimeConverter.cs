5bde8a65065d071423a904f3316dc6dfbdb361cf
Artūrs Eimanis <arturseimanis@gmail.com>
2017-09-13 18:07:07 +0300

    :construction: Make DateTimeToTimeConverter time zone aware (#562)

--- Toggl.Foundation.MvvmCross/Converters/DateTimeToTimeConverter.cs
+++ Toggl.Foundation.MvvmCross/Converters/DateTimeToTimeConverter.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Globalization;
 using MvvmCross.Platform.Converters;
 
 namespace Toggl.Foundation.MvvmCross.Converters
 {
     public sealed class DateTimeToTimeConverter : MvxValueConverter<DateTimeOffset, string>
     {
-        protected override string Convert(DateTimeOffset value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-            => value.ToString("HH:mm");
+        protected override string Convert(DateTimeOffset value, Type targetType, object parameter, CultureInfo culture)
+            => TimeZoneInfo.ConvertTime(value, TimeZoneInfo.Local).ToString("HH:mm");
     }
 }
