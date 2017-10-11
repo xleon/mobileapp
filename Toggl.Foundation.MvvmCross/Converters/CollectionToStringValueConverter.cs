cdea1ad2d536a188bc96b40214ab0e034ea215c9
Artūrs Eimanis <arturseimanis@gmail.com>
2017-10-11 14:17:00 +0300

    :construction: Fix CollectionToStringValueConverter (#701)

--- Toggl.Foundation.MvvmCross/Converters/CollectionToStringValueConverter.cs
+++ Toggl.Foundation.MvvmCross/Converters/CollectionToStringValueConverter.cs
@@ -10,10 +10,11 @@ namespace Toggl.Foundation.MvvmCross.Converters
     public sealed class CollectionToStringValueConverter<T> : MvxValueConverter<IEnumerable<T>, string>
     {
         protected override string Convert(IEnumerable<T> value, Type targetType, object parameter, CultureInfo culture)
-            => value.Aggregate(
-                   new StringBuilder(),
-                   (builder, str) => builder.Append($" {str.ToString()}"))
-               .Remove(0, 1)
-               .ToString();
+            => value == null || !value.Any() ? ""
+            : value.Aggregate(
+                  new StringBuilder(),
+                  (builder, str) => builder.Append($" {str.ToString()}"))
+              .Remove(0, 1)
+              .ToString();
     }
 }
