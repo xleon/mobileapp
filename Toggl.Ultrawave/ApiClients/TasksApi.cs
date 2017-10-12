4e3efdbf2fc5f58ae386393a9f0c5134f676ba30
Simon Rozsival <simon@rozsival.com>
2017-10-12 10:38:38 +0200

    :fire: Remove outdated piece of code (#707)

--- Toggl.Ultrawave/ApiClients/TasksApi.cs
+++ Toggl.Ultrawave/ApiClients/TasksApi.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Reactive.Linq;
 using Toggl.Multivac.Models;
 using Toggl.Ultrawave.Models;
 using Toggl.Ultrawave.Network;
@@ -29,22 +28,7 @@ namespace Toggl.Ultrawave.ApiClients
             var endPoint = endPoints.Post(task.WorkspaceId, task.ProjectId);
             var taskCopy = task as Task ?? new Task(task);
             var observable = CreateObservable(endPoint, AuthHeader, taskCopy, SerializationReason.Post);
-            return observable.Select(recieved => temporaryFixOfAnApiBug(task, recieved));
+            return observable;
         }
-
-        private ITask temporaryFixOfAnApiBug(ITask sentToApi, ITask receivedFromApi)
-            => new Task
-            {
-                Id = receivedFromApi.Id,
-                Name = receivedFromApi.Name,
-                ProjectId = receivedFromApi.ProjectId,
-                WorkspaceId = receivedFromApi.WorkspaceId,
-                UserId = receivedFromApi.UserId,
-                EstimatedSeconds = receivedFromApi.EstimatedSeconds,
-                Active = receivedFromApi.Active,
-
-                At = receivedFromApi.At == default(DateTimeOffset) ? sentToApi.At : default(DateTimeOffset),
-                TrackedSeconds = sentToApi.TrackedSeconds
-            };
     }
 }
