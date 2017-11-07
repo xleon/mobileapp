1035318f5476f6eca19bde41a65c8b506f18a375
Simon Rozsival <simon@rozsival.com>
2017-11-07 16:47:01 +0100

    :sparkles: Invalid transition state (#919)
    * :sparkles: Add invalid transition state
    
    * :white_check_mark: Add tests
    
    * :bug: Add new files to the projects
    
    * :white_check_mark: Fix tests
    
    * :fire:
    
    * :white_check_mark: Add test case
    
    * :white_check_mark: Fix property test

---
+++ Toggl.Foundation.Tests/Sync/States/InvalidTransitionStateTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using FsCheck.Xunit;
+using Toggl.Foundation.Sync.States;
+using Xunit;
+using FluentAssertions;
+using FsCheck;
+
+namespace Toggl.Foundation.Tests.Sync.States
+{
+    public sealed class InvalidTransitionStateTests
+    {
+        [Fact]
+        public void ThrowsWhenTheConstructorArgumentIsNull()
+        {
+            Action creatingState = () => new InvalidTransitionState(null);
+
+            creatingState.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("          ")]
+        public void ThrowsWhenTheConstructorArgumentIsEmpty(string message)
+        {
+            Action creatingState = () => new InvalidTransitionState(message);
+
+            creatingState.ShouldThrow<ArgumentException>();
+        }
+
+        [Fact]
+        public void DoesNotThrowWhenNobodySubscribesToTheReturnedObservable()
+        {
+            var state = new InvalidTransitionState("Error.");
+
+            state.Start();
+        }
+
+        [Fact]
+        public void ThrowsInvalidOperationException()
+        {
+            Exception caughtException = null;
+            var state = new InvalidTransitionState("Some message");
+
+            var observable = state.Start();
+            observable.Subscribe(_ => { }, (Exception exception) => caughtException = exception);
+
+            caughtException.Should().NotBeNull();
+            caughtException.Should().BeOfType<InvalidOperationException>();
+        }
+
+        [Property]
+        public void ThrowsInvalidOperationExceptionWithASpecificMessage(NonEmptyString message)
+        {
+            if (message.Get.Trim().Length == 0) return;
+
+            Exception caughtException = null;
+            var state = new InvalidTransitionState(message.Get);
+
+            var observable = state.Start();
+            observable.Subscribe(_ => { }, (Exception exception) => caughtException = exception);
+
+            caughtException.Message.Should().Be($"Invalid transition: {message}");
+        }
+    }
+}
