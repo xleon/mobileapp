7fdc8ed6c35119a8d5b1d125af112aa528656b56
Artūrs Eimanis <arturseimanis@gmail.com>
2017-11-17 13:39:36 +0200

    :sparkles: Add clear text button to tags view (#959)

--- Toggl.Daneel/ViewControllers/SelectTagsViewController.designer.cs
+++ Toggl.Daneel/ViewControllers/SelectTagsViewController.designer.cs
@@ -1,55 +1,54 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
 
 namespace Toggl.Daneel.ViewControllers
 {
-    [Register ("SelectTagsViewController")]
-    partial class SelectTagsViewController
-    {
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton CloseButton { get; set; }
+	[Register ("SelectTagsViewController")]
+	partial class SelectTagsViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton CloseButton { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton SaveButton { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton SaveButton { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UITableView TagsTableView { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UITableView TagsTableView { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UITextField TextField { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UITextField TextField { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (CloseButton != null) {
+				CloseButton.Dispose ();
+				CloseButton = null;
+			}
 
-        void ReleaseDesignerOutlets ()
-        {
-            if (CloseButton != null) {
-                CloseButton.Dispose ();
-                CloseButton = null;
-            }
+			if (SaveButton != null) {
+				SaveButton.Dispose ();
+				SaveButton = null;
+			}
 
-            if (SaveButton != null) {
-                SaveButton.Dispose ();
-                SaveButton = null;
-            }
+			if (TagsTableView != null) {
+				TagsTableView.Dispose ();
+				TagsTableView = null;
+			}
 
-            if (TagsTableView != null) {
-                TagsTableView.Dispose ();
-                TagsTableView = null;
-            }
-
-            if (TextField != null) {
-                TextField.Dispose ();
-                TextField = null;
-            }
-        }
-    }
+			if (TextField != null) {
+				TextField.Dispose ();
+				TextField = null;
+			}
+		}
+	}
 }
