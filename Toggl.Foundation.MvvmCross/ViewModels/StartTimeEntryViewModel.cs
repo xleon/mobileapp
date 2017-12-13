f0d627a02341d428a46224554bc27667be6b9f0e
Simon Rozsival <simon@rozsival.com>
2017-12-13 10:30:44 +0100

    :fire: Remove the case when there is no suggestion in project suggestion mode (#1148)

--- Toggl.Foundation.MvvmCross/ViewModels/StartTimeEntryViewModel.cs
+++ Toggl.Foundation.MvvmCross/ViewModels/StartTimeEntryViewModel.cs
@@ -473,16 +473,6 @@ namespace Toggl.Foundation.MvvmCross.ViewModels
             IEnumerable<AutocompleteSuggestion> suggestions)
         {
             var firstSuggestion = suggestions.FirstOrDefault();
-            if (firstSuggestion == null && IsSuggestingProjects && TextFieldInfo.WorkspaceId.HasValue)
-            {
-                // This ensures that "No Project" suggestion is shown even when no project
-                // suggestion matches the search query.
-                var collection = new WorkspaceGroupedCollection<AutocompleteSuggestion>(
-                    "", new[] { ProjectSuggestion.NoProject(TextFieldInfo.WorkspaceId.Value, "") });
-
-                return new[] { collection };
-            }
-                
             if (firstSuggestion is ProjectSuggestion)
                 return suggestions.GroupByWorkspaceAddingNoProject();
 
