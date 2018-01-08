ccd6619ca5a153c3ac32102de67413a2695dcbf2
William Barbosa <william.sb@hotmail.com>
2018-01-08 15:27:10 -0200

    :white_check_mark: Add platform unit test projects (#1205)
    * :white_check_mark: Add platform unit test projects
    
    * :green_heart: Update .sln file

---
+++ Toggl.Giskard.Tests/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Android.App;
+using Android.OS;
+using Xunit.Sdk;
+using Xunit.Runners.UI;
+using Xunit;
+using FluentAssertions;
+
+namespace Toggl.Giskard.Tests
+{
+    [Activity(Label = "xUnit Android Runner", MainLauncher = true, Theme = "@android:style/Theme.Material.Light")]
+    public class MainActivity : RunnerActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            AddTestAssembly(Assembly.GetExecutingAssembly());
+            AddExecutionAssembly(typeof(ExtensibilityPointFactory).Assembly);
+            AutoStart = true;
+            //TerminateAfterExecution = true;
+            base.OnCreate(bundle);
+        }
+    }
+}
+
