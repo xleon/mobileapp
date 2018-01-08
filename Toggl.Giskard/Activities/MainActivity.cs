e3124082b41386095a2b865cef97f93c42191d44
William Barbosa <william.sb@hotmail.com>
2018-01-08 08:54:36 -0200

    :sparkles: Hide suggestions when none is available (#1243)

--- Toggl.Giskard/Activities/MainActivity.cs
+++ Toggl.Giskard/Activities/MainActivity.cs
@@ -1,6 +1,7 @@
 using Android.App;
 using Android.OS;
 using Android.Support.V7.Widget;
+using Android.Views;
 using MvvmCross.Droid.Support.V7.AppCompat;
 using MvvmCross.Droid.Views.Attributes;
 using Toggl.Foundation.MvvmCross.ViewModels;
@@ -11,12 +12,15 @@ namespace Toggl.Giskard.Activities
     [Activity(Theme = "@style/AppTheme")]
     public sealed class MainActivity : MvxAppCompatActivity<MainViewModel>
     {
+        public View SuggestionsContainer { get; set; }
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.MainActivity);
 
             OverridePendingTransition(Resource.Animation.abc_fade_in, Resource.Animation.abc_fade_out);
+            SuggestionsContainer = FindViewById(Resource.Id.MainSuggestionsContainer);
 
             SetSupportActionBar(FindViewById<Toolbar>(Resource.Id.Toolbar));
             SupportActionBar.SetDisplayHomeAsUpEnabled(false);
--- Toggl.Giskard/Fragments/SuggestionsFragment.cs
+++ Toggl.Giskard/Fragments/SuggestionsFragment.cs
@@ -1,20 +1,44 @@
-﻿using Android.OS;
+﻿using System.Collections.Specialized;
+using System.Linq;
+using Android.OS;
 using Android.Views;
+using Android.Widget;
 using MvvmCross.Binding.Droid.BindingContext;
 using MvvmCross.Droid.Support.V4;
 using MvvmCross.Droid.Views.Attributes;
 using Toggl.Foundation.MvvmCross.ViewModels;
+using Toggl.Giskard.Activities;
+using Toggl.Giskard.Bindings;
 
 namespace Toggl.Giskard.Fragments
 {
     [MvxFragmentPresentation(typeof(MainViewModel), Resource.Id.MainSuggestionsContainer)]
-    public sealed class SuggestionsFragment : MvxFragment
+    public sealed class SuggestionsFragment : MvxFragment<SuggestionsViewModel>
     {
+        private int containerHeightInPixels;
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             base.OnCreateView(inflater, container, savedInstanceState);
             var view = this.BindingInflate(Resource.Layout.SuggestionsFragment, null);
+
+            containerHeightInPixels = (int)130.DpToPixels(Activity);
+            ViewModel.Suggestions.CollectionChanged += onCollectionChanged;
+            calculateContainerHeight();
             return view;
+        }
+
+        private void onCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            calculateContainerHeight();
+        }
+
+        private void calculateContainerHeight()
+        {
+            var mainActivity = (MainActivity)Activity;
+            var height = ViewModel.Suggestions.Any() ? containerHeightInPixels : 0;
+            var newParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, height);
+            mainActivity.SuggestionsContainer.LayoutParameters = newParams;
         }
     }
 }
