54dfbe9f16f01793596a3ff7d1daefd639d35456
William Barbosa <william.sb@hotmail.com>
2018-01-11 10:15:23 -0200

    :art: Add start time entry layout files (#1266)
    * :art: Add start time entry layout files
    
    * :green_heart:

---
+++ Toggl.Giskard/Activities/StartTimeEntryActivity.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.Widget;
+using Android.Views;
+using MvvmCross.Droid.Support.V7.AppCompat;
+using MvvmCross.Droid.Views.Attributes;
+using Toggl.Foundation.MvvmCross.ViewModels;
+
+namespace Toggl.Giskard.Activities
+{
+    [MvxActivityPresentation]
+    [Activity(Theme = "@style/AppTheme")]
+    public sealed class StartTimeEntryActivity : MvxAppCompatActivity<StartTimeEntryViewModel>
+    {
+        public View SuggestionsContainer { get; set; }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.StartTimeEntryActivity);
+
+            OverridePendingTransition(Resource.Animation.abc_slide_in_bottom, Resource.Animation.abc_fade_out);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.Toolbar);
+
+            SetSupportActionBar(toolbar);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.SetDisplayShowHomeEnabled(true);
+
+            toolbar.NavigationClick += navigationClick;
+        }
+
+        public override void Finish()
+        {
+            base.Finish();
+            OverridePendingTransition(Resource.Animation.abc_fade_in, Resource.Animation.abc_slide_out_bottom);
+        }
+
+        private void navigationClick(object sender, Toolbar.NavigationClickEventArgs e)
+        {
+            ViewModel.BackCommand.Execute();
+        }
+    }
+}
