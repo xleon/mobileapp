d6462e69b1947251bacdf648cd1f7c61cd9e8afc
William Barbosa <william.sb@hotmail.com>
2018-01-25 11:02:35 -0200

    :construction: Refactor segmented recycler adapter logic (#1316)
    * :construction: Refactor segmented recycler adapter logic
    
    * :fire:

---
+++ Toggl.Giskard/Adapters/SegmentedRecyclerAdapter.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using Android.Runtime;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Droid.Support.V7.RecyclerView;
+
+namespace Toggl.Giskard.Adapters
+{
+    public abstract class SegmentedRecyclerAdapter<TCollection, TItem> : MvxRecyclerAdapter
+        where TCollection : MvxObservableCollection<TItem>
+    {
+        private readonly object headerListLock = new object();
+        private readonly List<int> headerIndexes = new List<int>();
+
+        private int? cachedItemCount;
+
+        protected abstract MvxObservableCollection<TCollection> Collection { get; }
+
+        public SegmentedRecyclerAdapter()
+        {
+        }
+
+        public SegmentedRecyclerAdapter(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer)
+        {
+        }
+
+        protected override void SetItemsSource(IEnumerable value)
+        {
+            base.SetItemsSource(value);
+
+            calculateHeaderIndexes();
+        }
+
+        protected override void OnItemsSourceCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            base.OnItemsSourceCollectionChanged(sender, e);
+
+            calculateHeaderIndexes();
+        }
+
+        public override object GetItem(int viewPosition)
+        {
+            try
+            {
+                var item = tryGetItem(viewPosition);
+                return item;
+            }
+            catch
+            {
+                calculateHeaderIndexes();
+                var item = tryGetItem(viewPosition);
+                return item;
+            }
+        }
+
+        public override int ItemCount
+        {
+            get
+            {
+                if (cachedItemCount == null)
+                {
+                    var collection = Collection;
+                    if (collection == null) return 0;
+
+                    var itemCount = collection.Aggregate(collection.Count, (acc, g) => acc + g.Count);
+                    cachedItemCount = itemCount;
+                }
+
+                return cachedItemCount.Value;
+            }
+        }
+
+        private void calculateHeaderIndexes()
+        {
+            lock (headerListLock)
+            {
+                cachedItemCount = null;
+                headerIndexes.Clear();
+
+                var collection = Collection;
+                if (collection == null) return;
+
+                var index = 0;
+                foreach (var nestedCollection in collection)
+                {
+                    headerIndexes.Add(index);
+                    index += nestedCollection.Count + 1;
+                }
+            }
+        }
+
+        private object tryGetItem(int viewPosition)
+        {
+            var collection = Collection;
+            if (collection == null)
+                return null;
+
+            var groupIndex = headerIndexes.IndexOf(viewPosition);
+            if (groupIndex >= 0)
+                return collection[groupIndex];
+
+            var currentGroupIndex = headerIndexes.FindLastIndex(index => index < viewPosition);
+            var offset = headerIndexes[currentGroupIndex] + 1;
+            var indexInGroup = viewPosition - offset;
+            return collection[currentGroupIndex][indexInGroup];
+        }
+    }
+}
--- Toggl.Giskard/Adapters/TimeEntriesLogRecyclerAdapter.cs
+++ Toggl.Giskard/Adapters/TimeEntriesLogRecyclerAdapter.cs
@@ -1,14 +1,9 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
 using Android.Runtime;
 using Android.Support.V7.Widget;
 using Android.Views;
 using MvvmCross.Binding.Droid.BindingContext;
 using MvvmCross.Core.ViewModels;
-using MvvmCross.Droid.Support.V7.RecyclerView;
 using Toggl.Foundation.MvvmCross.Collections;
 using Toggl.Foundation.MvvmCross.ViewModels;
 using Toggl.Giskard.TemplateSelectors;
@@ -16,13 +11,9 @@ using Toggl.Giskard.Views;
 
 namespace Toggl.Giskard.Adapters
 {
-    public sealed class TimeEntriesLogRecyclerAdapter : MvxRecyclerAdapter
+    public sealed class TimeEntriesLogRecyclerAdapter 
+        : SegmentedRecyclerAdapter<TimeEntryViewModelCollection, TimeEntryViewModel>
     {
-        private readonly object headerListLock = new object();
-        private readonly List<int> headerIndexes = new List<int>();
-
-        private int? cachedItemCount;
-
         public IMvxAsyncCommand<TimeEntryViewModel> ContinueCommand { get; set; }
 
         public TimeEntriesLogRecyclerAdapter()
@@ -34,86 +25,8 @@ namespace Toggl.Giskard.Adapters
         {
         }
 
-        protected override void SetItemsSource(IEnumerable value)
-        {
-            base.SetItemsSource(value);
-
-            calculateHeaderIndexes();
-        }
-
-        protected override void OnItemsSourceCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            base.OnItemsSourceCollectionChanged(sender, e);
-
-            calculateHeaderIndexes();
-        }
-
-        private void calculateHeaderIndexes()
-        {
-            lock (headerListLock)
-            {
-                cachedItemCount = null;
-                headerIndexes.Clear();
-
-                var timeEntryGroups = getTimeEntryGroups();
-                if (timeEntryGroups == null) return;
-
-                var index = 0;
-                foreach (var timeEntryGroup in timeEntryGroups)
-                {
-                    headerIndexes.Add(index);
-                    index += timeEntryGroup.Count + 1;
-                }
-            }
-        }
-
-        public override object GetItem(int viewPosition)
-        {
-            try
-            {
-                var timeEntryGroups = ItemsSource as MvxObservableCollection<TimeEntryViewModelCollection>;
-                if (timeEntryGroups == null || viewPosition == ItemCount - 1)
-                    return null;
-
-                var groupIndex = headerIndexes.IndexOf(viewPosition);
-                if (groupIndex >= 0)
-                    return timeEntryGroups[groupIndex];
-
-                var currentGroupIndex = headerIndexes.FindLastIndex(index => index < viewPosition);
-                var offset = headerIndexes[currentGroupIndex] + 1;
-                var indexInGroup = viewPosition - offset;
-                return timeEntryGroups[currentGroupIndex][indexInGroup];
-            }
-            catch
-            {
-                calculateHeaderIndexes();
-                return GetItem(viewPosition);
-            }
-        }
-
-        public override int ItemCount
-        {
-            get
-            {
-                if (cachedItemCount == null)
-                {
-                    var timeEntryGroups = getTimeEntryGroups();
-                    if (timeEntryGroups == null) return 0;
-
-                    var itemCount = timeEntryGroups.Aggregate(timeEntryGroups.Count, (acc, g) => acc + g.Count);
-                    cachedItemCount = itemCount + 1;
-                }
-
-                return cachedItemCount.Value;
-            }
-        }
-
-        internal void ContinueTimeEntry(int viewPosition)
-        {
-            var timeEntry = GetItem(viewPosition) as TimeEntryViewModel;
-            if (timeEntry == null) return;
-            ContinueCommand?.ExecuteAsync(timeEntry);
-        }
+        protected override MvxObservableCollection<TimeEntryViewModelCollection> Collection
+            => ItemsSource as MvxObservableCollection<TimeEntryViewModelCollection>;
 
         public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
         {
@@ -143,7 +56,21 @@ namespace Toggl.Giskard.Adapters
             }
         }
 
-        private MvxObservableCollection<TimeEntryViewModelCollection> getTimeEntryGroups()
-            => ItemsSource as MvxObservableCollection<TimeEntryViewModelCollection>;
+        public override int ItemCount => base.ItemCount + 1;
+
+        public override object GetItem(int viewPosition)
+        {
+            if (viewPosition == ItemCount - 1)
+                return null;
+
+            return base.GetItem(viewPosition);
+        }
+
+        internal void ContinueTimeEntry(int viewPosition)
+        {
+            var timeEntry = GetItem(viewPosition) as TimeEntryViewModel;
+            if (timeEntry == null) return;
+            ContinueCommand?.ExecuteAsync(timeEntry);
+        }
     }
 }
