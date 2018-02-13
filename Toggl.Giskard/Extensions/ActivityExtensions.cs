cadfa00f938390620b9c38423e797a5bfdff4da8
William Barbosa <william.sb@hotmail.com>
2018-02-13 10:39:20 -0200

    * :triangular_ruler: Status Bar colors (#1425)
    * :triangular_ruler: Status bar colors
    
    * :construction: Refactor availability checks

---
+++ Toggl.Giskard/Extensions/ActivityExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Graphics;
+using Android.Views;
+using Toggl.Giskard.Helper;
+
+namespace Toggl.Giskard.Extensions
+{
+    public static class ActivityExtensions
+    {
+        public static void ChangeStatusBarColor(this Activity activity, Color color, bool useDarkIcons = false)
+        {
+            var window = activity.Window;
+            window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+            window.ClearFlags(WindowManagerFlags.TranslucentStatus);
+            window.SetStatusBarColor(color);
+
+            if (MarshmallowApis.AreNotAvailable) return;
+
+            window.DecorView.SystemUiVisibility =
+                (StatusBarVisibility)(useDarkIcons ? SystemUiFlags.LightStatusBar : SystemUiFlags.Visible);
+        }
+    }
+}
