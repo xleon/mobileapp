d540b03bde844dcb3875095eb78fa85b7ef88ac6
Artūrs Eimanis <arturseimanis@gmail.com>
2018-02-19 13:14:13 +0200

    :construction: Add date format struct (#1539)

---
+++ Toggl.Multivac/DateFormat.cs
@@ -0,0 +1,50 @@
+﻿namespace Toggl.Multivac
+{
+    public struct DateFormat
+    {
+        /// <summary>
+        /// Intended for displaying in the UI
+        /// </summary>
+        public string Localized { get; }
+
+        /// <summary>
+        /// Intended for  using in dateTime.ToString(useHere)
+        /// </summary>
+        public string Long { get; }
+
+        /// <summary>
+        /// Same as DateFormat.Long, but without the year portion
+        /// </summary>
+        public string Short { get; }
+
+        private DateFormat(
+            string longDateFormat,
+            string shortDateFormat,
+            string localizedDateFormat)
+        {
+            Ensure.Argument.IsNotNull(longDateFormat, nameof(longDateFormat));
+            Ensure.Argument.IsNotNull(shortDateFormat, nameof(shortDateFormat));
+            Ensure.Argument.IsNotNull(localizedDateFormat, nameof(localizedDateFormat));
+
+            Long = longDateFormat;
+            Short = shortDateFormat;
+            Localized = localizedDateFormat;
+        }
+
+        public static DateFormat FromLocalizedDateFormat(string localizedDateFormat)
+        {
+            var longDateFormat = localizedDateFormat
+                .Replace('Y', 'y')
+                .Replace('D', 'd');
+
+            var shortDateFormat = longDateFormat
+                .Replace("y", "")
+                .Trim('.', '-', '/');
+
+            return new DateFormat(
+                longDateFormat,
+                shortDateFormat,
+                localizedDateFormat);
+        }
+    }
+}
---
+++ Toggl.Ultrawave/Serialization/Converters/DateFormatConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+using Toggl.Multivac;
+
+namespace Toggl.Ultrawave.Serialization.Converters
+{
+    [Preserve(AllMembers = true)]
+    public sealed class DateFormatConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+            => objectType == typeof(DateFormat);
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
+            => DateFormat.FromLocalizedDateFormat(reader.Value.ToString());
+
+        public override void WriteJson(JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            var dateFormat = (DateFormat)value;
+            writer.WriteValue(dateFormat.Localized);
+        }
+    }
+}
