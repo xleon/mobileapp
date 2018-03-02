09ac2dc061a19252c2549ac21eb3f2ba0936cdd2
Simon Rozsival <simon@rozsival.com>
2018-03-02 13:36:08 +0100

    :triangular_ruler: Fix jumping cursor (#1606)

--- Toggl.Daneel/Binding/TextFieldSecureTextEntryTargetBinding.cs
+++ Toggl.Daneel/Binding/TextFieldSecureTextEntryTargetBinding.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using CoreGraphics;
 using MvvmCross.Binding;
 using MvvmCross.Binding.Bindings.Target;
 using UIKit;
@@ -28,12 +27,9 @@ namespace Toggl.Daneel.Binding
 
         protected override void SetValue(bool value)
         {
-            //This is a dirty workaround because UITextField has a bug.
-            //Blame Apple.
+            Target.ResignFirstResponder();
             Target.SecureTextEntry = value;
-
-            if (!value) return;
-            Target.InsertText(Target.Text);
+            Target.BecomeFirstResponder();
         }
 
         private void onEditingDidBegin(object sender, EventArgs e)
