39556ae3678b5559d459c456d401aa126932d81f
William Barbosa <william.sb@hotmail.com>
2018-05-10 18:10:34 +0200

    :arrows_counterclockwise: Arrow rotation for tasks (#2099)

---
+++ Toggl.Giskard/Bindings/ViewUpsideDownTargetBinding.cs
@@ -0,0 +1,24 @@
+﻿using Android.Views;
+using MvvmCross.Binding;
+using MvvmCross.Binding.Droid.Target;
+
+namespace Toggl.Giskard.Bindings
+{
+    public sealed class ViewUpsideDownTargetBinding : MvxAndroidTargetBinding<View, bool>
+    {
+        public const string BindingName = "UpsideDown";
+
+        public override MvxBindingMode DefaultMode => MvxBindingMode.OneWay;
+
+        public ViewUpsideDownTargetBinding(View target)
+                : base(target)
+        {
+        }
+
+        protected override void SetValueImpl(View target, bool value)
+        {
+            var angle = value ? 180.0f : 0.0f;
+            target.Animate().SetDuration(1).Rotation(angle);
+        }
+    }
+}
--- Toggl.Giskard/Startup/SetupTargetFactories.cs
+++ Toggl.Giskard/Startup/SetupTargetFactories.cs
@@ -50,11 +50,6 @@ namespace Toggl.Giskard
                 view => new FabVisibilityTargetBinding(view)
             );
 
-            registry.RegisterCustomBindingFactory<ImageView>(
-                ImageViewVerticalFlipTargetBinding.BindingName,
-                view => new ImageViewVerticalFlipTargetBinding(view)
-            );
-
             registry.RegisterCustomBindingFactory<TextView>(
                 TextViewFontWeightTargetBinding.BindingName,
                 view => new TextViewFontWeightTargetBinding(view)
@@ -73,6 +68,11 @@ namespace Toggl.Giskard
             registry.RegisterCustomBindingFactory<ViewPager>(
                 ViewPagerCurrentPageTargetBinding.BindingName,
                 view => new ViewPagerCurrentPageTargetBinding(view)
+            );
+
+            registry.RegisterCustomBindingFactory<View>(
+                ViewUpsideDownTargetBinding.BindingName,
+                view => new ViewUpsideDownTargetBinding(view)
             );
 
             registry.RegisterCustomBindingFactory<View>(
