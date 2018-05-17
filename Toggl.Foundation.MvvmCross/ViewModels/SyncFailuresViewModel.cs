6a9675393d6ce269ffbb9235dec64953dc77501d
Ricardo Sánchez Sotres <odrakir@gmail.com>
2018-05-17 13:26:20 +0200

    :sparkles: Sync failures viewmodel (#2164)
    * :sparkles: Add SyncFailuresViewModel
    
    * :white_check_mark: Add view model tests
    
    * _:construction: Make interactor access awaitable
    
    * :white_check_mark: Add test for view model property
    
    * Make property set accessor private
    
    * :lipstick: Fix typo
    
    * :lipstick: Correct lines arrangement
    
    * _:construction: Change property type to IEnumerable
    
    * :fire: Remove using directive

---
+++ Toggl.Foundation.MvvmCross/ViewModels/SyncFailuresViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using MvvmCross.Core.ViewModels;
+using Toggl.Multivac;
+using Toggl.Foundation.Interactors;
+using Toggl.Foundation.Models;
+
+namespace Toggl.Foundation.MvvmCross.ViewModels
+{
+    [Preserve(AllMembers = true)]
+    public class SyncFailuresViewModel : MvxViewModel
+    {
+        public IEnumerable<SyncFailureItem> SyncFailures { get; private set; }
+
+        private readonly IInteractorFactory interactorFactory;
+
+        public SyncFailuresViewModel(IInteractorFactory interactorFactory)
+        {
+            Ensure.Argument.IsNotNull(interactorFactory, nameof(interactorFactory));
+
+            this.interactorFactory = interactorFactory;
+        }
+
+        public override async Task Initialize()
+        {
+            await base.Initialize();
+
+            SyncFailures = await interactorFactory
+                .GetItemsThatFailedToSync().Execute()
+                .FirstAsync();
+        }
+    }
+}
---
+++ Toggl.Foundation.Tests/MvvmCross/ViewModels/SyncFailuresViewModelTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using FluentAssertions;
+using NSubstitute;
+using Toggl.Foundation.Interactors;
+using Toggl.Foundation.Models;
+using Toggl.Foundation.MvvmCross.ViewModels;
+using Toggl.Foundation.Tests.Generators;
+using Toggl.Foundation.Tests.Mocks;
+using Toggl.PrimeRadiant.Models;
+using Xunit;
+
+namespace Toggl.Foundation.Tests.MvvmCross.ViewModels
+{
+    public sealed class SyncFailuresViewModelTests
+    {
+        public abstract class SyncFailuresViewModelTest : BaseViewModelTests<SyncFailuresViewModel>
+        {
+            protected override SyncFailuresViewModel CreateViewModel()
+            {
+                var vm = new SyncFailuresViewModel(InteractorFactory);
+                return vm;
+            }
+        }
+
+        public sealed class TheConstructor : SyncFailuresViewModelTest
+        {
+            [Fact, LogIfTooSlow]
+            public void ThrowsIfTheArgumentIsNull()
+            {
+                Action tryingToConstructWithEmptyParameters =
+                    () => new SyncFailuresViewModel(null);
+
+                tryingToConstructWithEmptyParameters
+                    .ShouldThrow<ArgumentNullException>();
+            }
+        }
+
+        public sealed class TheSyncFailuresProperty : SyncFailuresViewModelTest
+        {
+            [Fact, LogIfTooSlow]
+            public void ExecutesGetItemsThatFailedInteractor()
+            {
+                var mockedInteractor = Substitute.For<IInteractor<IObservable<IEnumerable<SyncFailureItem>>>>();
+                InteractorFactory.GetItemsThatFailedToSync().Returns(mockedInteractor);
+
+                ViewModel.Initialize();
+
+                mockedInteractor.Received().Execute();
+            }
+
+            [Fact, LogIfTooSlow]
+            public void ReturnsTheInteractorResultAsAList()
+            {
+                var mockedInteractor = Substitute.For<IInteractor<IObservable<IEnumerable<SyncFailureItem>>>>();
+                InteractorFactory.GetItemsThatFailedToSync().Returns(mockedInteractor);
+
+                SyncFailureItem[] result = {
+                    new SyncFailureItem(new MockProject { Name = "MyProject" }),
+                    new SyncFailureItem(new MockProject { Name = "MyProject 2" }),
+                    new SyncFailureItem(new MockTag { Name = "MyTag" })
+                };
+
+                mockedInteractor.Execute().Returns(Observable.Return(result));
+
+                ViewModel.Initialize();
+
+                ViewModel.SyncFailures.Should().BeEquivalentTo(result);
+            }
+        }
+    }
+}
