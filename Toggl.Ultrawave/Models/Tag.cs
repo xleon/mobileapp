a7f6268c7dc698d8f1a1e194997957b0b10fb6ba
Simon Rozsival <simon@rozsival.com>
2018-05-17 19:06:35 +0300

    :rewind: Revert tag deserialization change (#2183)
    * Revert ":fire: Remove ugly serialization code (#2163)"
    
    This reverts commit d77f97218a692f631ccf6e867f2cc9e0ff851837.
    
    * :construction: Set an explicitly fake date as default At date
    
    * :construction:

--- Toggl.Ultrawave/Models/Tag.cs
+++ Toggl.Ultrawave/Models/Tag.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using Toggl.Multivac.Models;
+using Newtonsoft.Json;
 
 namespace Toggl.Ultrawave.Models
 {
     internal sealed partial class Tag : ITag
     {
+        private static readonly DateTimeOffset defaultAt = new DateTimeOffset(2000, 01, 02, 03, 04, 05, TimeSpan.Zero);
+
         public long Id { get; set; }
 
         public long WorkspaceId { get; set; }
@@ -14,5 +17,15 @@ namespace Toggl.Ultrawave.Models
         public DateTimeOffset At { get; set; }
 
         public DateTimeOffset? DeletedAt { get; set; }
+
+        [JsonConstructor]
+        public Tag(long id, long workspaceId, string name, DateTimeOffset? at, DateTimeOffset? deletedAt)
+        {
+            Id = id;
+            WorkspaceId = workspaceId;
+            Name = name;
+            At = at ?? defaultAt;
+            DeletedAt = deletedAt;
+        }
     }
 }
