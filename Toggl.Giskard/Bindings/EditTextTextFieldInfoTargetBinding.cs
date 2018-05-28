33e2a6b3cc41068584b3693d21a409877e19237e
daividssilverio <daivid.silverio@gmail.com>
2018-05-28 11:48:39 -0400

    🐛 Fix new cursor position when adding tags/projects on start TE (#2229)
    * :bug: Fix new cursor position when adding tags/projects on start TE
    
    * :fire: Remove unused usings
    
    * :fire: Remove unused property
    
    * :construction: Extract EBMs
    
    * :construction: Extract count occurences string extension
    
    * :see_no_evil: Fix english spelling
    
    * :construction: Move CountOccurrences to Multivac
    
    * :fire:
    
    * :fire: Revert changes to shared code
    
    * :bug: Fix android cursor behavior when adding tags/projects
    
    Now without changes to the shared code
    
    * :lipstick: Remove empty line

--- Toggl.Giskard/Bindings/EditTextTextFieldInfoTargetBinding.cs
+++ Toggl.Giskard/Bindings/EditTextTextFieldInfoTargetBinding.cs
@@ -6,6 +6,7 @@ using Toggl.Foundation.Autocomplete;
 using Toggl.Foundation.MvvmCross.Autocomplete;
 using Toggl.Giskard.Autocomplete;
 using Toggl.Giskard.Extensions;
+using Toggl.Multivac.Extensions;
 
 namespace Toggl.Giskard.Bindings
 {
@@ -19,8 +20,12 @@ namespace Toggl.Giskard.Bindings
 
         protected override IAutocompleteEventProvider EventProvider => spanWatcher;
 
+        private bool shouldUseLastKnownPosition;
+
+        private int lastKnownPosition;
+
         public EditTextTextFieldInfoTargetBinding(EditText target)
-                : base(target)
+            : base(target)
         {
             Target.AddTextChangedListener(spanWatcher);
         }
@@ -29,7 +34,9 @@ namespace Toggl.Giskard.Bindings
             => Target.GetDescription();
 
         protected override int GetCurrentCursorPosition()
-            => Target.SelectionStart;
+            => Target.SelectionStart == 0 && shouldUseLastKnownPosition
+                ? lastKnownPosition
+                : Target.SelectionStart;
 
         protected override bool CheckIfSelectingText()
             => Target.SelectionStart != Target.SelectionEnd;
@@ -43,6 +50,8 @@ namespace Toggl.Giskard.Bindings
             if (Target.TextFormatted.Length() == formattedText.Length())
                 return;
 
+            lastKnownPosition = Target.SelectionStart;
+            shouldUseLastKnownPosition = isProbablyEditingTokenRegion(textFieldInfo.Text);
             Target.TextFormatted = formattedText;
             Target.SetSelection(textFieldInfo.CursorPosition);
         }
@@ -51,5 +60,28 @@ namespace Toggl.Giskard.Bindings
         {
             Target.Invalidate();
         }
+
+        private bool isProbablyEditingTokenRegion(string newDescription)
+            => isProbablyEditingProjectRegion(newDescription)
+               || isProbablyEditingTagsRegion(newDescription);
+
+        private bool isProbablyEditingProjectRegion(string newDescription)
+            => isNewDescriptionAddingToken(newDescription, QuerySymbols.ProjectsString)
+               || isNumberOfProjectTokensSmallerThanBefore(newDescription);
+
+        private bool isNumberOfProjectTokensSmallerThanBefore(string newDescription)
+            => newDescription.CountOccurrences(QuerySymbols.Projects) <
+               (Target.Text?.CountOccurrences(QuerySymbols.Projects) ?? 0);
+
+        private bool isProbablyEditingTagsRegion(string newDescription)
+            => isNewDescriptionAddingToken(newDescription, QuerySymbols.TagsString)
+               || isNumberOfTagTokensDifferentThanBefore(newDescription);
+
+        private bool isNumberOfTagTokensDifferentThanBefore(string newDescription)
+            => newDescription.CountOccurrences(QuerySymbols.Tags) <
+               (Target.Text?.CountOccurrences(QuerySymbols.Tags) ?? 0);
+
+        private bool isNewDescriptionAddingToken(string newDescription, string token)
+            => $"{Target.Text?.Trim() ?? string.Empty} {token}" == newDescription.Trim();
     }
 }
