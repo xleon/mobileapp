524bc48e690466ec0066457172613c485f006748
Simon Rozsival <simon@rozsival.com>
2018-05-30 16:38:33 +0200

    :construction: Bump DB schema version (#2337)

--- Toggl.PrimeRadiant.Realm/Database.cs
+++ Toggl.PrimeRadiant.Realm/Database.cs
@@ -74,15 +74,15 @@ namespace Toggl.PrimeRadiant.Realm
         private RealmConfiguration createRealmConfiguration()
             => new RealmConfiguration
             {
-                SchemaVersion = 3,
+                SchemaVersion = 4,
                 MigrationCallback = (migration, oldSchemaVersion) =>
                 {
-                    if (oldSchemaVersion < 2)
+                    if (oldSchemaVersion < 3)
                     {
-                        // nothing needs explicit updating when updating form schema 0 to 1 and from 1 to 2
+                        // nothing needs explicit updating when updating form schema 0 up to 3
                     }
 
-                    if (oldSchemaVersion < 3)
+                    if (oldSchemaVersion < 4)
                     {
                         var newTags = migration.NewRealm.All<RealmTag>();
                         var oldTags = migration.OldRealm.All("RealmTag");
