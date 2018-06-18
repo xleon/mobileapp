9a04cb0e1dcd5c8667b097e78c852c26eb7212a0
Ricardo Sánchez Sotres <odrakir@gmail.com>
2018-06-18 18:24:35 +0200

    :sparkles: Add datepicker rx extensions (#2487)

--- Toggl.Daneel/Extensions/UIExtensions.cs
+++ Toggl.Daneel/Extensions/UIExtensions.cs
@@ -24,6 +24,23 @@ namespace Toggl.Daneel.Extensions
                 return Disposable.Create(() => view.RemoveGestureRecognizer(gestureRecognizer));
             });
 
+        public static IObservable<DateTimeOffset> DateChanged(this UIDatePicker datePicker)
+            => Observable
+                .FromEventPattern(e => datePicker.ValueChanged += e, e => datePicker.ValueChanged -= e)
+                .Select(e => ((UIDatePicker) e.Sender).Date.ToDateTimeOffset());
+
+        public static IObservable<DateTimeOffset> DateComponentChanged(this UIDatePicker datePicker)
+            => datePicker.DateChanged()
+                .StartWith(datePicker.Date.ToDateTimeOffset())
+                .DistinctUntilChanged(d => d.Date)
+                .Skip(1);
+
+        public static IObservable<DateTimeOffset> TimeComponentChanged(this UIDatePicker datePicker)
+            => datePicker.DateChanged()
+                .StartWith(datePicker.Date.ToDateTimeOffset())
+                .DistinctUntilChanged(d => d.TimeOfDay)
+                .Skip(1);
+
         public static Action<bool> BindIsVisible(this UIView view)
             => isVisible => view.Hidden = !isVisible;
 
