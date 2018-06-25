562d33021d1a60cb417ca80c52dc561118943363
daividssilverio <daivid.silverio@gmail.com>
2018-06-25 19:38:18 -0400

    🎨 Uniformise Android settings UI Items (#2539)
    * :art: Replace buttons with textviews to make them uniform
    
    * :wrench: Update view fields
    
    * :lipstick:
    
    * :lipstick:
    
    * :lipstick: Last touch

--- Toggl.Giskard/Activities/SettingsActivity.ViewBinds.cs
+++ Toggl.Giskard/Activities/SettingsActivity.ViewBinds.cs
@@ -7,21 +7,20 @@ namespace Toggl.Giskard.Activities
 {
     public partial class SettingsActivity
     {
+        private View helpView;
+        private View aboutView;
+        private View logoutView;
+        private View feedbackView;
         private View manualModeView;
         private View avatarContainer;
         private View beginningOfWeekView;
-        private View aboutContainer;
 
         private TextView nameTextView;
         private TextView emailTextView;
-        private TextView beginningOfWeekTextView;
         private TextView versionTextView;
+        private TextView beginningOfWeekTextView;
 
         private ImageView avatarView;
-
-        private Button helpButton;
-        private Button logoutButton;
-        private Button feedbackButton;
 
         private Switch manualModeSwitch;
 
@@ -29,19 +28,18 @@ namespace Toggl.Giskard.Activities
 
         protected override void InitializeViews()
         {
+            helpView = FindViewById(Resource.Id.SettingsHelpButton);
+            aboutView = FindViewById(Resource.Id.SettingsAboutContainer);
+            logoutView = FindViewById(Resource.Id.SettingsLogoutButton);
+            feedbackView = FindViewById(Resource.Id.SettingsSubmitFeedbackButton);
             manualModeView = FindViewById(Resource.Id.SettingsToggleManualModeView);
             avatarContainer = FindViewById(Resource.Id.SettingsViewAvatarImageContainer);
             beginningOfWeekView = FindViewById(Resource.Id.SettingsSelectBeginningOfWeekView);
-            aboutContainer = FindViewById(Resource.Id.SettingsAboutContainer);
 
             nameTextView = FindViewById<TextView>(Resource.Id.SettingsNameTextView);
             emailTextView = FindViewById<TextView>(Resource.Id.SettingsEmailTextView);
-            beginningOfWeekTextView = FindViewById<TextView>(Resource.Id.SettingsBeginningOfWeekTextView);
             versionTextView = FindViewById<TextView>(Resource.Id.SettingsAppVersionTextView);
-
-            helpButton = FindViewById<Button>(Resource.Id.SettingsHelpButton);
-            logoutButton = FindViewById<Button>(Resource.Id.SettingsLogoutButton);
-            feedbackButton = FindViewById<Button>(Resource.Id.SettingsSubmitFeedbackButton);
+            beginningOfWeekTextView = FindViewById<TextView>(Resource.Id.SettingsBeginningOfWeekTextView);
 
             avatarView = FindViewById<ImageView>(Resource.Id.SettingsViewAvatarImage);
             manualModeSwitch = FindViewById<Switch>(Resource.Id.SettingsIsManualModeEnabledSwitch);
--- Toggl.Giskard/Activities/SettingsActivity.cs
+++ Toggl.Giskard/Activities/SettingsActivity.cs
@@ -53,12 +53,12 @@ namespace Toggl.Giskard.Activities
                 avatarContainer.Visibility = ViewStates.Visible;
             });
 
-            this.Bind(logoutButton.Tapped(), ViewModel.TryLogout);
-            this.Bind(helpButton.Tapped(), ViewModel.ShowHelpView);
-            this.Bind(feedbackButton.Tapped(), ViewModel.SubmitFeedback);
+            this.Bind(logoutView.Tapped(), ViewModel.TryLogout);
+            this.Bind(helpView.Tapped(), ViewModel.ShowHelpView);
+            this.Bind(aboutView.Tapped(), ViewModel.OpenAboutView);
+            this.Bind(feedbackView.Tapped(), ViewModel.SubmitFeedback);
             this.BindVoid(manualModeView.Tapped(), ViewModel.ToggleManualMode);
             this.Bind(beginningOfWeekView.Tapped(), ViewModel.SelectBeginningOfWeek);
-            this.Bind(aboutContainer.Tapped(), ViewModel.OpenAboutView);
 
             setupToolbar();
         }
