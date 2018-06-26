941b4bc0d5f6e6bb6f0ad641bf31a01cd58fe02f
Simon Rozsival <simon@rozsival.com>
2018-06-26 19:53:20 +0200

    :triangular_ruler: Fix duration field ellipsis (#2535)
    * :construction: Simplify the construction of the attributed string
    
    * :construction: Update function call
    
    * :lipstick: Change field name
    
    * :construction: Move prefix length code to Multivac
    
    * :white_check_mark: Add tests
    
    * :bug: Fix the regexp
    
    * :white_check_mark: Add more tests
    
    * :triangular_ruler: Update size of the duration input
    
    * :construction: Set font color of the higlighted part

---
+++ Toggl.Multivac/DurationHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Toggl.Multivac
+{
+    public static class DurationHelper
+    {
+        private static readonly Regex prefixExpression = new Regex("^[0:]*(:|$)", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
+        public static int LengthOfDurationPrefix(string durationText)
+        {
+            var match = prefixExpression.Match(durationText);
+            return match.Success ? match.Length : 0;
+        }
+    }
+}
