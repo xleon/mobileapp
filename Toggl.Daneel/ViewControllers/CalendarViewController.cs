3e5cdea735c94e9177465f2b4cb5ab8c1365f72a
Artūrs Eimanis <arturseimanis@gmail.com>
2018-08-06 17:59:35 +0300

    :art: Add CalendarViewController (#2835)

---
+++ Toggl.Daneel/ViewControllers/CalendarViewController.cs
@@ -0,0 +1,15 @@
+﻿using MvvmCross.Platforms.Ios.Presenters.Attributes;
+using Toggl.Foundation.MvvmCross.ViewModels;
+
+namespace Toggl.Daneel.ViewControllers
+{
+    [MvxChildPresentation]
+    public sealed partial class CalendarViewController
+        : ReactiveViewController<CalendarViewModel>
+    {
+        public CalendarViewController() : base(null)
+        {
+        }
+    }
+}
+
