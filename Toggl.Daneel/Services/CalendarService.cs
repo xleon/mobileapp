7bdfca20f5cc2f4fc70e834318bde5b669138709
Juan Laube <juanlaube@users.noreply.github.com>
2018-08-13 14:22:36 -0300

    :apple: Fetch calendar events (#2916)

--- Toggl.Daneel/Services/CalendarService.cs
+++ Toggl.Daneel/Services/CalendarService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
 using EventKit;
+using Toggl.Daneel.Extensions;
 using Toggl.Foundation.Calendar;
 using Toggl.Foundation.MvvmCross.Services;
 using Toggl.Multivac;
@@ -19,18 +20,52 @@ namespace Toggl.Daneel.Services
         }
 
         public override IObservable<IEnumerable<CalendarItem>> GetEventsForDate(DateTime date)
-            => Observable.Return(new List<CalendarItem>());
+        {
+            var startOfDay = new DateTimeOffset(date.Date);
+            var startDate = startOfDay.ToNSDate();
+            var endDate = startOfDay.AddDays(1).ToNSDate();
+
+            var calendars = eventStore.GetCalendars(EKEntityType.Event);
+
+            var predicate = eventStore
+                .PredicateForEvents(startDate, endDate, calendars);
+
+            var calendarItems = eventStore
+                .EventsMatching(predicate)
+                .Where(isValidEvent)
+                .Select(calendarItemFromEvent);
+
+            return Observable.Return(calendarItems);
+        }
 
         protected override IEnumerable<UserCalendar> NativeGetUserCalendars()
             => eventStore
                 .GetCalendars(EKEntityType.Event)
                 .Select(userCalendarFromEKCalendar);
-        
+
         private UserCalendar userCalendarFromEKCalendar(EKCalendar calendar)
             => new UserCalendar(
                 calendar.CalendarIdentifier,
                 calendar.Title,
-                calendar.Source.Title
+                calendar.Source.Title);
+
+        private CalendarItem calendarItemFromEvent(EKEvent ev)
+        {
+            var startDate = ev.StartDate.ToDateTimeOffset();
+            var endDate = ev.EndDate.ToDateTimeOffset();
+            var duration = endDate - startDate;
+
+            return new CalendarItem(
+                CalendarItemSource.Calendar,
+                startDate,
+                duration,
+                ev.Title,
+                ev.Calendar.CGColor.ToHexColor(),
+                calendarId: ev.Calendar.CalendarIdentifier
             );
+        }
+
+        private bool isValidEvent(EKEvent ev)
+            => !ev.AllDay;
     }
 }
