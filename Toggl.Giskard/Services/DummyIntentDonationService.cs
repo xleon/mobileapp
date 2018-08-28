d9e4a855c35bc60c97302a28548d7bc4dfbb77a3
khoi <khoiracle@gmail.com>
2018-08-28 12:12:50 +0700

    🚧 DummyIntentService for Giskard

---
+++ Toggl.Giskard/Services/DummyIntentDonationService.cs
@@ -0,0 +1,12 @@
+using Toggl.Foundation.Services;
+
+namespace Toggl.Giskard.Services
+{
+    public class DummyIntentDonationService: IIntentDonationService
+    {
+        public void DonateStopCurrentTimeEntry()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
