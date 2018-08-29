36e5514a60cc25aa305dbb3768f17f932adacf7e
Ricardo Sánchez Sotres <odrakir@gmail.com>
2018-08-29 17:10:17 +0200

    :package: Add ultrawave to the Siri Extension (#3110)
    * Add ultrawave reference
    
    * Instantiate TogglApi in extension
    
    * Change agent name
    
    * Change Agent name again

--- Toggl.Daneel.SiriExtension/IntentHandler.cs
+++ Toggl.Daneel.SiriExtension/IntentHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using Toggl.Ultrawave;
+using Toggl.Ultrawave.Network;
 using Toggl.Daneel.Intents;
 using Foundation;
 using Intents;
@@ -9,6 +10,12 @@ namespace SiriExtension
     [Register("IntentHandler")]
     public class IntentHandler : INExtension
     {
+        #if USE_PRODUCTION_API
+        private const ApiEnvironment environment = ApiEnvironment.Production;
+        #else
+        private const ApiEnvironment environment = ApiEnvironment.Staging;
+        #endif
+
         protected IntentHandler(IntPtr handle) : base(handle)
         {
             // Note: this .ctor should not contain any initialization logic.
@@ -19,7 +26,12 @@ namespace SiriExtension
             var bundleId = NSBundle.MainBundle.BundleIdentifier;
             bundleId = bundleId.Substring(0, bundleId.LastIndexOf("."));
             var userDefaults = new NSUserDefaults($"group.{bundleId}.extensions", NSUserDefaultsType.SuiteName);
-           
+
+            var version = NSBundle.MainBundle.InfoDictionary["CFBundleShortVersionString"].ToString();
+            var apiToken = userDefaults.StringForKey("api-token");
+            var userAgent = new UserAgent("Daneel", $"{version}.SiriExtension");
+            var togglApi = new TogglApi(new ApiConfiguration(environment, Credentials.WithApiToken(apiToken), userAgent));
+
             if (intent is StopTimerIntent)
             {
                 return new StopTimerIntentHandler();
