431f6863c50ff7afb22eee4b1491c8d0709dc7f9
Simon Rozsival <simon@rozsival.com>
2018-09-12 14:31:02 +0200

    :sparkles: Reports bar charts API client (#3209)
    * :sparkles: Add API client
    
    * :sparkles: Implement API client logic
    
    * :white_check_mark: Add integration tests

---
+++ Toggl.Ultrawave/Models/Reports/TimeEntriesTotals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Toggl.Multivac;
+using Toggl.Multivac.Models.Reports;
+
+namespace Toggl.Ultrawave.Models.Reports
+{
+    [Preserve(AllMembers = true)]
+    public sealed class TimeEntriesTotals : ITimeEntriesTotals
+    {
+        public DateTimeOffset StartDate { get; set; }
+
+        public DateTimeOffset? EndDate { get; set; }
+
+        public Resolution Resolution { get; set; }
+
+        public ITimeEntriesTotalsGroup[] Groups { get; set; }
+    }
+}
