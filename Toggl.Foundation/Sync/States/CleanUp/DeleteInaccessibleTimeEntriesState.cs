ae3a9c4565b41a3dfb7e8eaa7ff059eca651d618
Juan Laube <juanlaube@users.noreply.github.com>
2018-10-17 12:51:23 -0300

    :fire: Add state to delete synced ghost time entries (#3476)

---
+++ Toggl.Foundation/Sync/States/CleanUp/DeleteInaccessibleTimeEntriesState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reactive.Linq;
+using Toggl.Foundation.DataSources;
+using Toggl.Multivac;
+using Toggl.PrimeRadiant;
+using Toggl.PrimeRadiant.Models;
+
+namespace Toggl.Foundation.Sync.States.CleanUp
+{
+    public class DeleteInaccessibleTimeEntriesState : ISyncState
+    {
+        private readonly ITimeEntriesSource dataSource;
+
+        public StateResult FinishedDeleting { get; } = new StateResult();
+
+        public DeleteInaccessibleTimeEntriesState(ITimeEntriesSource dataSource)
+        {
+            Ensure.Argument.IsNotNull(dataSource, nameof(dataSource));
+            this.dataSource = dataSource;
+        }
+
+        public IObservable<ITransition> Start()
+            => dataSource
+                .GetAll(suitableForDeletion, includeGhosts: true)
+                .SelectMany(dataSource.DeleteAll)
+                .Select(_ => FinishedDeleting.Transition());
+
+        private bool suitableForDeletion(IDatabaseTimeEntry timeEntry)
+            => timeEntry.IsInaccessible && isSynced(timeEntry);
+
+        private bool isSynced(IDatabaseTimeEntry timeEntry)
+            => timeEntry.SyncStatus == SyncStatus.InSync;
+    }
+}
