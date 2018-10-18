25177168471a61eaa70b1780c0bad4a627fab424
Simon Rozsival <simon@rozsival.com>
2018-10-18 12:41:03 +0200

    :sparkles: Add UIAction factory method (#3485)

--- Toggl.Multivac/Extensions/RxAction/UIAction.cs
+++ Toggl.Multivac/Extensions/RxAction/UIAction.cs
@@ -1,20 +1,37 @@
 using System;
 using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 
 namespace Toggl.Multivac.Extensions
 {
-    public class UIAction : RxAction<Unit, Unit>
+    public sealed class UIAction : RxAction<Unit, Unit>
     {
-        public UIAction(Func<IObservable<Unit>> workFactory) : base(_ => workFactory())
+        public UIAction(Func<IObservable<Unit>> workFactory)
+            : base(_ => workFactory())
         {
         }
 
-        public UIAction(Func<IObservable<Unit>> workFactory, IObservable<bool> enabledIf) : base(_ => workFactory(), enabledIf)
+        public UIAction(Func<IObservable<Unit>> workFactory, IObservable<bool> enabledIf)
+            : base(_ => workFactory(), enabledIf)
         {
         }
 
         public IObservable<Unit> Execute()
             => Execute(Unit.Default);
+
+        public static UIAction FromAction(Action action)
+        {
+            IObservable<Unit> workFactory()
+                => Observable.Create<Unit>(observer =>
+                {
+                    action();
+                    observer.CompleteWith(Unit.Default);
+                    return Disposable.Empty;
+                });
+
+            return new UIAction(workFactory);
+        }
     }
 
     public static class RxActionExtensions
