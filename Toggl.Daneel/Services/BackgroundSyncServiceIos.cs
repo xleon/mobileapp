66ef99ce971ba357f5930f58b4e0038a5bb5d459
Simon Rozsival <simon@rozsival.com>
2019-01-09 16:41:46 +0100

    :apple: Always change background sync configuration on main thread (#4124)

--- Toggl.Daneel/Services/BackgroundSyncServiceIos.cs
+++ Toggl.Daneel/Services/BackgroundSyncServiceIos.cs
@@ -6,11 +6,21 @@ namespace Toggl.Daneel.Services
     public sealed class BackgroundSyncServiceIos : BaseBackgroundSyncService
     {
         public override void EnableBackgroundSync()
-            => UIApplication.SharedApplication
-                .SetMinimumBackgroundFetchInterval(MinimumBackgroundFetchInterval.TotalSeconds);
+        {
+            configureInterval(MinimumBackgroundFetchInterval.TotalSeconds);
+        }
 
         public override void DisableBackgroundSync()
-            => UIApplication.SharedApplication
-                .SetMinimumBackgroundFetchInterval(UIApplication.BackgroundFetchIntervalNever);
+        {
+            configureInterval(UIApplication.BackgroundFetchIntervalNever);
+        }
+
+        private static void configureInterval(double interval)
+        {
+            UIApplication.SharedApplication.InvokeOnMainThread(() =>
+            {
+                UIApplication.SharedApplication.SetMinimumBackgroundFetchInterval(interval);
+            });
+        }
     }
 }
