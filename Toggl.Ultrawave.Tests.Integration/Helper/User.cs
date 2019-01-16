87cac4060a6568d0df035d1a1c6eab572600b628
Simon Rozsival <simon@rozsival.com>
2019-01-16 12:21:34 +0100

    :construction: Move delay from between tests to after creating user entity (#4208)

--- Toggl.Ultrawave.Tests.Integration/Helper/User.cs
+++ Toggl.Ultrawave.Tests.Integration/Helper/User.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using Toggl.Multivac.Extensions;
@@ -41,7 +42,21 @@ namespace Toggl.Ultrawave.Tests.Integration
         private static async Task<IUser> createUser(Email email, Password password)
         {
             var api = Helper.TogglApiFactory.TogglApiWith(Credentials.None);
-            return await api.User.SignUp(email, password, true, 237);
+            var user = await api.User.SignUp(email, password, true, 237);
+
+            // This is to make integration tests run slightly slower to prevent SecureChannelFailure
+            // errors caused by too many HTTP calls in quick succession in most cases
+            // (combined with limiting the XUnit max parallel threads)
+            // Empirically, a delay of 0.5s is too short, while 2s sees no further improvements
+            //
+            // Another issue this is trying to solve is the replication lag between databases.
+            // This causes problems when we create a new user and try to make more requests with the credentials
+            // of this user which is not known to all the replicas. We then end up with 403 Forbidden
+            // if the following request hits a replica which doesn't know the user. There is no perfect
+            // solution but we are told that the replication lag should be under one second in most cases.
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            return user;
         }
     }
 }
