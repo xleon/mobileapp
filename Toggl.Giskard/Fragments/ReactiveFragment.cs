2c0809e52ebaa44d947ebf9b18b6c4ed766dd743
Juan Laube <juanlaube@users.noreply.github.com>
2019-02-12 08:46:41 -0300

    :construction: Convert the SettingsActivity into a Fragment (#4379)

---
+++ Toggl.Giskard/Fragments/ReactiveFragment.cs
@@ -0,0 +1,21 @@
+﻿using System.Reactive.Disposables;
+using Android.OS;
+using MvvmCross.Droid.Support.V4;
+using MvvmCross.ViewModels;
+
+namespace Toggl.Giskard.Fragments
+{
+    public class ReactiveFragment<TViewModel> : MvxFragment<TViewModel>
+        where TViewModel : class, IMvxViewModel
+    {
+        protected CompositeDisposable DisposeBag = new CompositeDisposable();
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            if (!disposing) return;
+            DisposeBag?.Dispose();
+        }
+    }
+}
