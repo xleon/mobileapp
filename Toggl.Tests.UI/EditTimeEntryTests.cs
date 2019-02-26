2c79669acb0ad1d89f211ee12516d5efd98fa22f
Jordan <Jordanteasdale@hotmail.com>
2019-02-26 20:26:41 +0100

    Add new cs for edit time entry tests (#4471)
    * :white_check_mark: Update AndroidAccessibilityNames
    
    Updates the name of those forgot password field
    
    * :white_check_mark: Add EditTimeEntryfile
    
    Adds a file to work on the EditTimeEntryTests so they are categorised in there.
    
    * :white_check_mark: add EditTimeEntry.cs
    
    Adds a new file for categorisation of UI tests

---
+++ Toggl.Tests.UI/EditTimeEntryTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+using static Toggl.Tests.UI.Extensions.MainExtensions;
+using static Toggl.Tests.UI.Extensions.StartTimeEntryExtensions;
+
+namespace Toggl.Tests.UI
+{
+    public class EditTimeEntryTests
+    {
+        public EditTimeEntryTests()
+        {
+        }
+    }
+}
