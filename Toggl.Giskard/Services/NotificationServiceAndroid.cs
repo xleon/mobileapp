4345ee2953d558ea22bdf4616a52238ee4e8de8d
daividssilverio <daivid.silverio@gmail.com>
2019-02-27 11:49:48 -0400

    :bug: Calendroid: Fix notification scheduler (#4479)
    * :sparkles: Add smart alert calendar event broadcast receiver
    
    * :construction: Fix notification service to actually schedule notitifications
    
    * :construction: Remove unnecessary Select

--- Toggl.Giskard/Services/NotificationServiceAndroid.cs
+++ Toggl.Giskard/Services/NotificationServiceAndroid.cs
@@ -1,48 +1,63 @@
 ﻿using System;
 using System.Collections.Immutable;
-using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
 using Android.App;
+using Android.Content;
+using Android.OS;
 using Android.Support.V4.App;
 using Toggl.Foundation.Services;
+using Toggl.Giskard.BroadcastReceivers;
 using Toggl.Giskard.Extensions;
-using Toggl.Multivac.Extensions;
+using Notification = Android.App.Notification;
 
 namespace Toggl.Giskard.Services
 {
     public sealed class NotificationServiceAndroid : INotificationService
     {
-
         public IObservable<Unit> Schedule(IImmutableList<Multivac.Notification> notifications)
             => Observable.Start(() =>
-                {
-                    var context = Application.Context;
-                    var notificationIntent = context.PackageManager.GetLaunchIntentForPackage(context.PackageName);
-                    notificationIntent.SetPackage(null);
-                    var pendingIntent = PendingIntent.GetActivity(context, 0, notificationIntent, 0);
-                    var notificationManager = NotificationManager.FromContext(context);
-                    var notificationsBuilder = context.CreateNotificationBuilderWithDefaultChannel(notificationManager);
+            {
+                var context = Application.Context;
+                var notificationIntent = context.PackageManager.GetLaunchIntentForPackage(context.PackageName);
+                notificationIntent.SetPackage(null);
+                var pendingIntent = PendingIntent.GetActivity(context, 0, notificationIntent, 0);
+                var notificationManager = NotificationManager.FromContext(context);
+                var notificationsBuilder = context.CreateNotificationBuilderWithDefaultChannel(notificationManager);
 
-                    notifications.Select(notification =>
-                        (
-                            notification.Id.GetHashCode(), // this is a temp measure, might be better to change this in the future
-                            notificationsBuilder.SetAutoCancel(true)
-                                .SetContentIntent(pendingIntent)
-                                .SetContentTitle(notification.Title)
-                                .SetSmallIcon(Resource.Drawable.ic_icon_running)
-                                .SetContentText(notification.Description)
-                                .Build()
-                        )
-                    ).ForEach(notificationManager.Notify);
-                });
+                foreach (var notification in notifications)
+                {
+                    var notificationId = notification.Id.GetHashCode(); // this is a temp measure, might be better to change this in the future
+                    var androidNotification = notificationsBuilder.SetAutoCancel(true)
+                        .SetContentIntent(pendingIntent)
+                        .SetContentTitle(notification.Title)
+                        .SetSmallIcon(Resource.Drawable.ic_icon_running)
+                        .SetContentText(notification.Description)
+                        .Build();
+                    var scheduleAt = notification.ScheduledTime;
+
+                    scheduleNotification(notificationId, androidNotification, scheduleAt);
+                }
+            });
 
         public IObservable<Unit> UnscheduleAllNotifications()
             => Observable.Start(() =>
-                {
-                    var context = Application.Context;
-                    var notificationManager = NotificationManagerCompat.From(context);
-                    notificationManager.CancelAll();
-                });
+            {
+                var context = Application.Context;
+                var notificationManager = NotificationManagerCompat.From(context);
+                notificationManager.CancelAll();
+            });
+
+        public static void scheduleNotification(int notificationId, Notification notification, DateTimeOffset scheduleAt)
+        {
+            var scheduledNotificationIntent = new Intent(Application.Context, typeof(SmartAlertCalendarEventBroadcastReceiver));
+            scheduledNotificationIntent.PutExtra(SmartAlertCalendarEventBroadcastReceiver.NotificationId, notificationId);
+            scheduledNotificationIntent.PutExtra(SmartAlertCalendarEventBroadcastReceiver.Notification, notification);
+            var pendingIntent = PendingIntent.GetBroadcast(Application.Context, notificationId, scheduledNotificationIntent, PendingIntentFlags.CancelCurrent);
+
+            var futureInMillis = (long) (scheduleAt - DateTimeOffset.Now).TotalMilliseconds;
+            var alarmManager = (AlarmManager) Application.Context.GetSystemService(Context.AlarmService);
+            alarmManager.SetExact(AlarmType.ElapsedRealtimeWakeup, SystemClock.ElapsedRealtime() + futureInMillis, pendingIntent);
+        }
     }
 }
