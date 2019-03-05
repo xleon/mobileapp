42304e7b35a769404f9372c76ceb67fa2907d91e
Juan Laube <juanlaube@users.noreply.github.com>
2019-03-05 16:46:31 -0300

    :art: Calendar header layout (#4525)

--- Toggl.Giskard/Fragments/CalendarFragment.ViewBinds.cs
+++ Toggl.Giskard/Fragments/CalendarFragment.ViewBinds.cs
@@ -6,6 +6,13 @@ namespace Toggl.Giskard.Fragments
 {
     public partial class CalendarFragment
     {
+        private View headerCalendarEventsView;
+        private View headerTimeEntriesView;
+        private TextView headerDayTextView;
+        private TextView headerWeekdayTextView;
+        private TextView headerCalendarEventsTextView;
+        private TextView headerTimeEntriesTextView;
+        private Button headerLinkCalendarsButton;
         private CalendarRecyclerView calendarRecyclerView;
         private ViewStub onboardingViewStub;
         private View onboardingView;
@@ -14,6 +21,13 @@ namespace Toggl.Giskard.Fragments
 
         protected override void InitializeViews(View view)
         {
+            headerCalendarEventsView = view.FindViewById(Resource.Id.HeaderCalendarEventsView);
+            headerTimeEntriesView = view.FindViewById(Resource.Id.HeaderTimeEntriesView);
+            headerDayTextView = view.FindViewById<TextView>(Resource.Id.Day);
+            headerWeekdayTextView = view.FindViewById<TextView>(Resource.Id.Weekday);
+            headerCalendarEventsTextView = view.FindViewById<TextView>(Resource.Id.HeaderCalendarEventsTextView);
+            headerTimeEntriesTextView = view.FindViewById<TextView>(Resource.Id.HeaderTimeEntriesTextView);
+            headerLinkCalendarsButton = view.FindViewById<Button>(Resource.Id.HeaderLinkCalendarsButton);
             calendarRecyclerView = view.FindViewById<CalendarRecyclerView>(Resource.Id.calendarRecyclerView);
             onboardingViewStub = view.FindViewById<ViewStub>(Resource.Id.OnboardingViewStub);
         }
