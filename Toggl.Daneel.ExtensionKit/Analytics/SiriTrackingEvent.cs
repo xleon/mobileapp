xleon/mobileapp

commit 197bfec7343c6a6fe10fbaa53a3250f625826ba3

📊 Track Siri actions under regular events (#4528)

Toggl.Daneel.ExtensionKit/Analytics/SiriTrackingEvent.cs
@@ -5,78 +5,94 @@ using Toggl.Multivac.Models;
 
 namespace Toggl.Daneel.ExtensionKit.Analytics
 {
+    public enum SiriTrackingEventType
+    {
+        StartTimer = 1,
+        StopTimer = 2,
+        Error = 3
+    }
+
+    public struct SiriErrorEventKeys
+    {
+        public static string Message = "Message";
+    }
+
+    public struct SiriStartTimerEventKeys
+    {
+        public static string HasEmptyDescription = "HasEmptyDescription";
+        public static string HasProject = "HasProject";
+        public static string HasTask = "HasTask";
+        public static string NumberOfTags = "NumberOfTags";
+        public static string IsBillable = "IsBillable";
+    }
+
     [Register("SiriTrackingEvent")]
     public class SiriTrackingEvent: NSObject, INSCoding
     {
-        private static readonly string eventNameEncodeKey = nameof(eventNameEncodeKey);
+        private static readonly string eventTypeEncodeKey = nameof(eventTypeEncodeKey);
         private static readonly string parametersEncodeKey = nameof(parametersEncodeKey);
 
-        private static readonly string SiriStartTimerEventName = "SiriStartTimer";
-        private static readonly string SiriStopTimerEventName = "SiriStopTimer";
-        private static readonly string SiriIntentErrorEventName = "SiriIntentError";
+        public readonly SiriTrackingEventType EventType;
+        public readonly Dictionary<string, string> Parameters;
 
-        public readonly NSString EventName;
-        public readonly NSDictionary<NSString, NSString> Parameters;
-
-        public SiriTrackingEvent(NSString eventName, NSDictionary<NSString, NSString> parameters)
+        public SiriTrackingEvent(SiriTrackingEventType eventType, Dictionary<string, string> parameters)
         {
-            EventName = eventName;
+            EventType = eventType;
             Parameters = parameters;
         }
 
         public static SiriTrackingEvent Error(string message)
         {
-            var dict = new Dictionary<string, string>
+            return new SiriTrackingEvent(SiriTrackingEventType.Error, new Dictionary<string, string>
             {
-                ["Message"] = message
-            };
-
-            var nativeDict = NSDictionary<NSString, NSString>
-                .FromObjectsAndKeys(dict.Values.ToArray(), dict.Keys.ToArray());
-
-            return new SiriTrackingEvent(new NSString(SiriIntentErrorEventName), nativeDict);
+                [SiriErrorEventKeys.Message] = message
+            });
         }
 
         public static SiriTrackingEvent StartTimer(ITimeEntry te)
         {
-            var dict = new Dictionary<string, string>
+            return new SiriTrackingEvent(SiriTrackingEventType.StartTimer, new Dictionary<string, string>
             {
-                ["HasEmptyDescription"] = string.IsNullOrEmpty(te.Description).ToString(),
-                ["HasProject"] = (te.ProjectId != null).ToString(),
-                ["HasTask"] = (te.TaskId != null).ToString(),
-                ["NumberOfTags"] = te.TagIds.Count().ToString(),
-                ["IsBillable"] = te.Billable.ToString()
-            };
-
-            var nativeDict = NSDictionary<NSString, NSString>
-                .FromObjectsAndKeys(dict.Values.ToArray(), dict.Keys.ToArray());
-
-            return new SiriTrackingEvent(new NSString(SiriStartTimerEventName), nativeDict);
+                [SiriStartTimerEventKeys.HasEmptyDescription] = string.IsNullOrEmpty(te.Description).ToString(),
+                [SiriStartTimerEventKeys.HasProject] = (te.ProjectId != null).ToString(),
+                [SiriStartTimerEventKeys.HasTask] = (te.TaskId != null).ToString(),
+                [SiriStartTimerEventKeys.NumberOfTags] = te.TagIds.Count().ToString(),
+                [SiriStartTimerEventKeys.IsBillable] = te.Billable.ToString()
+            });
         }
 
         public static SiriTrackingEvent StopTimer()
         {
-            return new SiriTrackingEvent(new NSString(SiriStopTimerEventName), null);
+            return new SiriTrackingEvent(SiriTrackingEventType.StopTimer, null);
         }
 
         #region INSCoding
         [Export("initWithCoder:")]
         public SiriTrackingEvent(NSCoder coder)
         {
-            EventName = (NSString) coder.DecodeObject(eventNameEncodeKey);
+            EventType = (SiriTrackingEventType) coder.DecodeInt(eventTypeEncodeKey);
 
-            var dict = (NSDictionary) coder.DecodeObject(parametersEncodeKey);
-            if (dict != null)
+            var nativeDict = (NSDictionary) coder.DecodeObject(parametersEncodeKey);
+
+            if (nativeDict != null)
             {
-                Parameters = NSDictionary<NSString, NSString>.FromObjectsAndKeys(dict.Values, dict.Keys);
+                var dict = new Dictionary<string, string>();
+                foreach (var item in nativeDict)
+                {
+                    dict.Add ((NSString)item.Key, (NSString)item.Value);
+                }
+                Parameters = dict;
             }
         }
 
         [Export ("encodeWithCoder:")]
         public void EncodeTo(NSCoder encoder)
         {
-            encoder.Encode(EventName, eventNameEncodeKey);
-            encoder.Encode(Parameters, parametersEncodeKey);
+            encoder.Encode((int)EventType, eventTypeEncodeKey);
+
+            var nativeDict = NSDictionary
+                .FromObjectsAndKeys(Parameters.Values.ToArray(), Parameters.Keys.ToArray());
+            encoder.Encode(nativeDict, parametersEncodeKey);
         }
         #endregion
     }

Toggl.Daneel/Analytics/SiriIntentErrorTrackingEvent.cs
@@ -1,0 +1,22 @@
+using System.Collections.Generic;
+using Toggl.Foundation.Analytics;
+
+namespace Toggl.Daneel
+{
+    public class SiriIntentErrorTrackingEvent: ITrackableEvent
+    {
+        public string EventName => "SiriIntentError";
+        private readonly string Message;
+
+        public SiriIntentErrorTrackingEvent(string message)
+        {
+            Message = message;
+        }
+
+        public Dictionary<string, string> ToDictionary()
+            => new Dictionary<string, string>
+            {
+                ["Message"] = Message
+            };
+    }
+}

Toggl.Daneel/Analytics/SiriTrackableEvent.cs
@@ -1,39 +1,0 @@
-using System.Collections.Generic;
-using Foundation;
-using Toggl.Daneel.ExtensionKit.Analytics;
-using Toggl.Foundation.Analytics;
-
-namespace Toggl.Daneel
-{
-    public class SiriTrackableEvent: ITrackableEvent
-    {
-        public string EventName => internalTrackingEvent.EventName;
-
-        public Dictionary<string, string> ToDictionary()
-        {
-            var dict = new Dictionary<string, string>();
-
-            if (internalTrackingEvent.Parameters != null)
-            {
-                foreach (var item in internalTrackingEvent.Parameters)
-                {
-                    dict.Add((NSString)item.Key, (NSString)item.Value);
-                }
-            }
-
-            return dict;
-        }
-
-        private SiriTrackingEvent internalTrackingEvent;
-        private NSDateFormatter dateFormatter = new NSDateFormatter()
-        {
-            DateFormat = "yyyy-MM-dd'T'HH:mm:ssZ",
-            TimeZone = NSTimeZone.FromGMT(0)
-        };
-
-        public SiriTrackableEvent(SiriTrackingEvent internalTrackingEvent)
-        {
-            this.internalTrackingEvent = internalTrackingEvent;
-        }
-    }
-}

Toggl.Daneel/Analytics/SiriTrackingEventExtensions.cs
@@ -1,0 +1,33 @@
+using System;
+using Foundation;
+using Toggl.Daneel.ExtensionKit.Analytics;
+using Toggl.Foundation.Analytics;
+
+namespace Toggl.Daneel
+{
+    public static class SiriTrackingEventExtensions
+    {
+        public static ITrackableEvent ToTrackableEvent(this SiriTrackingEvent e)
+        {
+            switch (e.EventType)
+            {
+                case SiriTrackingEventType.Error:
+                    return new SiriIntentErrorTrackingEvent(e.Parameters[SiriErrorEventKeys.Message]);
+                case SiriTrackingEventType.StartTimer:
+                    return new StartTimeEntryEvent(
+                        TimeEntryStartOrigin.Siri,
+                        Convert.ToBoolean(e.Parameters[SiriStartTimerEventKeys.HasEmptyDescription]),
+                        Convert.ToBoolean(e.Parameters[SiriStartTimerEventKeys.HasProject]),
+                        Convert.ToBoolean(e.Parameters[SiriStartTimerEventKeys.HasTask]),
+                        Convert.ToInt32(e.Parameters[SiriStartTimerEventKeys.NumberOfTags]),
+                        Convert.ToBoolean(e.Parameters[SiriStartTimerEventKeys.IsBillable]),
+                        true
+                        );
+                case SiriTrackingEventType.StopTimer:
+                    return new StopTimerEntryEvent(TimeEntryStopOrigin.Siri);
+                default:
+                    return null;
+            }
+        }
+    }
+}

Toggl.Daneel/Toggl.Daneel.csproj
@@ -663,7 +663,8 @@
     </None>
   </ItemGroup>
   <ItemGroup>
-    <Compile Include="Analytics\SiriTrackableEvent.cs" />
+    <Compile Include="Analytics\SiriIntentErrorTrackingEvent.cs" />
+    <Compile Include="Analytics\SiriTrackingEventExtensions.cs" />
     <Compile Include="Binding\DatePickerDateTimeOffsetTargetBinding.cs" />
     <Compile Include="Cells\BaseCollectionViewCell.cs" />
     <Compile Include="Extensions\Binders\ReactiveTableViewBinder.cs" />

Toggl.Daneel/ViewControllers/MainViewController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
@@ -308,10 +309,10 @@ namespace Toggl.Daneel.ViewControllers
         {
             var events = SharedStorage.instance.PopTrackableEvents();
 
-            foreach (var e in events)
-            {
-                ViewModel.Track(new SiriTrackableEvent(e));
-            }
+            events
+                .Select(e => e.ToTrackableEvent())
+                .Where(e => e != null)
+                .Do(ViewModel.Track);
         }
 
         private void onApplicationDidBecomeActive(NSNotification notification)

Toggl.Foundation/Analytics/StopTimerEntryEvent.cs
@@ -1,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace Toggl.Foundation.Analytics
+{
+    public sealed class StopTimerEntryEvent : ITrackableEvent
+    {
+        public string EventName => "TimeEntryStopped";
+        public TimeEntryStopOrigin Origin { get; }
+
+        public StopTimerEntryEvent(TimeEntryStopOrigin origin)
+        {
+            Origin = origin;
+        }
+
+        public Dictionary<string, string> ToDictionary() =>
+            new Dictionary<string, string>
+            {
+                [nameof(Origin)] = Origin.ToString(),
+            };
+    }
+}

Toggl.Foundation/Analytics/TimeEntryStartOrigin.cs
@@ -8,6 +8,7 @@
         Suggestion,
         ContinueMostRecent,
         CalendarEvent,
-        CalendarTapAndDrag
+        CalendarTapAndDrag,
+        Siri
     }
 }

commit 30377409ac3cb779289ee4cda2372fa140169527

💥 fix Siri stop intent crash (#4547)
## What's this?
fix a crash introduced by #4528 since the stop event can't be saved to the Shared Storage due to the Params being null.

### Side effects
none.

## Review hints
None.

## :squid: Permissions
🎙

Toggl.Daneel.ExtensionKit/Analytics/SiriTrackingEvent.cs
@@ -90,9 +90,12 @@ namespace Toggl.Daneel.ExtensionKit.Analytics
         {
             encoder.Encode((int)EventType, eventTypeEncodeKey);
 
-            var nativeDict = NSDictionary
+            if (Parameters != null)
+            {
+                var nativeDict = NSDictionary
                 .FromObjectsAndKeys(Parameters.Values.ToArray(), Parameters.Keys.ToArray());
-            encoder.Encode(nativeDict, parametersEncodeKey);
+                encoder.Encode(nativeDict, parametersEncodeKey);
+            }
         }
         #endregion
     }