30f3308f5076d1f3e1292567e040715911f82008
William Barbosa <william.sb@hotmail.com>
2019-05-01 12:59:54 -0300

    :fire: Remove MvvmCross from iOS startup

--- Toggl.iOS/Startup/AppDelegate.cs
+++ Toggl.iOS/Startup/AppDelegate.cs
@@ -1,19 +1,46 @@
+using System.Reactive;
 using Foundation;
+using Toggl.Core;
+using Toggl.Core.UI;
+using Toggl.Core.UI.ViewModels;
+using Toggl.iOS.Presentation;
+using Toggl.Networking;
 using UIKit;
 using UserNotifications;
 
 namespace Toggl.iOS
 {
     [Register(nameof(AppDelegate))]
-    public sealed class AppDelegate : MvxApplicationDelegate<Setup, App<OnboardingViewModel>>, IUNUserNotificationCenterDelegate
+    public sealed partial class AppDelegate : UIApplicationDelegate, IUNUserNotificationCenterDelegate
     {
+        private const ApiEnvironment environment =
+#if USE_PRODUCTION_API
+                        ApiEnvironment.Production;
+#else
+                        ApiEnvironment.Staging;
+#endif
+
         public override UIWindow Window { get; set; }
 
         public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
         {
+#if !USE_PRODUCTION_API
+            System.Net.ServicePointManager.ServerCertificateValidationCallback
+                  += (sender, certificate, chain, sslPolicyErrors) => true;
+#endif
+
             initializeAnalytics();
 
             base.FinishedLaunching(application, launchOptions);
+
+            var version = NSBundle.MainBundle.InfoDictionary["CFBundleShortVersionString"].ToString();
+            IosDependencyContainer.EnsureInitialized(new TogglPresenter(this, Window), environment, Platform.Daneel, version);
+            var app = new App<OnboardingViewModel, Unit>(IosDependencyContainer.Instance);
+
+            app.Start();
+
+            setupNavigationBar();
+            setupTabBar();
 
             UNUserNotificationCenter.Current.Delegate = this;
 
@@ -24,14 +51,6 @@ namespace Toggl.iOS
             return true;
         }
 
-        protected override void RunAppStart(object hint = null)
-        {
-            base.RunAppStart(hint);
-
-            setupNavigationBar();
-            setupTabBar();
-        }
-        
         #if USE_ANALYTICS
         public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
         {
