c8f7246376079d7adeee859e9b2ef406c7762b61
Juan Laube <juanlaube@users.noreply.github.com>
2019-05-06 10:03:48 -0300

    :construction: Implement IView for iOS (#5024)

--- Toggl.iOS/Services/PermissionsCheckerIos.cs
+++ Toggl.iOS/Services/PermissionsCheckerIos.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using EventKit;
 using Foundation;
 using Toggl.Core.UI.Services;
+using Toggl.iOS.Helper;
 using UIKit;
 using UserNotifications;
 
@@ -13,66 +14,24 @@ namespace Toggl.iOS.Services
     [Preserve(AllMembers = true)]
     public sealed class PermissionsCheckerIos : IPermissionsChecker
     {
-        private readonly UNAuthorizationOptions options = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
+        private readonly UNAuthorizationOptions options =
+            UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
 
         public IObservable<bool> CalendarPermissionGranted
-            => Observable.Return(
-                EKEventStore.GetAuthorizationStatus(EKEntityType.Event) == EKAuthorizationStatus.Authorized
-            );
+            => PermissionsHelper.CalendarPermissionGranted;
 
         public IObservable<bool> NotificationPermissionGranted
-            => UNUserNotificationCenter.Current
-                .GetNotificationSettingsAsync()
-                .ToObservable()
-                .Select(settings => settings.AuthorizationStatus == UNAuthorizationStatus.Authorized);
+            => PermissionsHelper.NotificationPermissionGranted;
 
         public IObservable<bool> RequestCalendarAuthorization(bool force = false)
-            => requestPermission(
-                CalendarPermissionGranted,
-                () => new EKEventStore().RequestAccessAsync(EKEntityType.Event),
-                force
-            );
+            => PermissionsHelper.RequestCalendarAuthorization(force);
 
         public IObservable<bool> RequestNotificationAuthorization(bool force = false)
-            => requestPermission(
-                NotificationPermissionGranted,
-                () => UNUserNotificationCenter.Current.RequestAuthorizationAsync(options),
-                force
-            );
+            => PermissionsHelper.RequestNotificationAuthorization(force);
 
         public void OpenAppSettings()
         {
-            UIApplication.SharedApplication.OpenUrl(
-                NSUrl.FromString(UIApplication.OpenSettingsUrlString)
-            );
+            PermissionsHelper.OpenAppSettings();
         }
-
-        private IObservable<bool> requestPermission(
-            IObservable<bool> permissionChecker,
-            Func<Task<Tuple<bool, NSError>>> permissionRequestFunction,
-            bool force)
-            => Observable.DeferAsync(async cancellationToken =>
-            {
-                var permissionGranted = await permissionChecker;
-
-                if (permissionGranted)
-                    return Observable.Return(true);
-
-                if (force)
-                {
-                    //Fact: If the user changes any permissions through the settings, this app gets restarted
-                    //and in that case we don't care about the value returned from this method.
-                    //We care about the returned value in the case, when user opens settings
-                    //and comes back to this app without altering any permissions. In that case
-                    //returning the current permission status is the correct behaviour.
-                    OpenAppSettings();
-                    return Observable.Return(false);
-                }
-
-                return Observable
-                    .FromAsync(permissionRequestFunction)
-                    .Select(tuple => tuple.Item1);
-            });
-
     }
 }
---
+++ Toggl.iOS/ViewControllers/Reactive/ReactiveTableViewController.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Reactive.Disposables;
+using System.Threading.Tasks;
+using Toggl.Core.UI.ViewModels;
+using Toggl.Core.UI.Views;
+using Toggl.iOS.Extensions;
+using UIKit;
+
+namespace Toggl.iOS.ViewControllers
+{
+    public abstract partial class ReactiveTableViewController<TViewModel> : UITableViewController, IView
+        where TViewModel : IViewModel
+    {
+        public CompositeDisposable DisposeBag { get; private set; } = new CompositeDisposable();
+
+        public TViewModel ViewModel { get; set; }
+
+        protected ReactiveTableViewController()
+            : base(null, null) { }
+
+        protected ReactiveTableViewController(IntPtr handle)
+            : base(handle) { }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            ViewModel?.AttachView(this);
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+            ViewModel?.ViewAppearing();
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+            ViewModel?.ViewAppeared();
+        }
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+            ViewModel?.ViewDisappearing();
+        }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+            ViewModel?.ViewDisappeared();
+        }
+
+        public Task Close()
+        {
+            this.Dismiss();
+            return Task.CompletedTask;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            if (!disposing) return;
+            DisposeBag?.Dispose();
+        }
+    }
+}
