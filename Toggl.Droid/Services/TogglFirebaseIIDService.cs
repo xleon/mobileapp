c06ca5adbf69b21e4f3b6e187e073c0c32119af1
daividssilverio <daivid.silverio@gmail.com>
2019-06-07 03:20:21 -0400

    :construction: Resubscribe to pushes when token changes (#5334)

--- Toggl.Droid/Services/TogglFirebaseIIDService.cs
+++ Toggl.Droid/Services/TogglFirebaseIIDService.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using Android.App;
 using Firebase.Iid;
 using Toggl.Core;
 using Toggl.Networking;
+using Toggl.Shared.Extensions;
 
 namespace Toggl.Droid.Services
 {
@@ -16,6 +20,8 @@ namespace Toggl.Droid.Services
             ApiEnvironment.Staging;
         #endif
 
+        private CompositeDisposable disposeBag = new CompositeDisposable();
+        
         public override void OnTokenRefresh()
         {
             var applicationContext = Application.Context;
@@ -26,6 +32,30 @@ namespace Toggl.Droid.Services
 
             var dependencyContainer = AndroidDependencyContainer.Instance;
             dependencyContainer.InteractorFactory.StoreNewTokenInteractor(token).Execute();
+
+            registerTokenIfNecessary(dependencyContainer);
+        }
+
+        private void registerTokenIfNecessary(AndroidDependencyContainer dependencyContainer)
+        {
+            var userLoggedIn = dependencyContainer.UserAccessManager.CheckIfLoggedIn();
+            if (!userLoggedIn) return;
+            
+            var subscribeToPushNotificationsInteractor = dependencyContainer.InteractorFactory.SubscribeToPushNotifications();
+            subscribeToPushNotificationsInteractor
+                .Execute()
+                .ObserveOn(dependencyContainer.SchedulerProvider.BackgroundScheduler)
+                .Subscribe(_ => StopSelf())
+                .DisposedBy(disposeBag);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            if (!disposing) return;
+            
+            disposeBag?.Dispose();
         }
     }
 }
