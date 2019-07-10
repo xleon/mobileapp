d436b830179e9ee0d6018cd6b907950a6f0ce124
Juan Laube <juanlaube@users.noreply.github.com>
2019-07-10 11:11:49 -0300

    :art: Implement timer widget design (#5564)

--- Toggl.iOS.TimerWidgetExtension/TodayViewController.designer.cs
+++ Toggl.iOS.TimerWidgetExtension/TodayViewController.designer.cs
@@ -1,18 +1,74 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
-
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace Toggl.iOS.TimerWidgetExtension
 {
-    [Register("TodayViewController")]
-    partial class TodayViewController
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("TodayViewController")]
+	partial class TodayViewController
+	{
+		[Outlet]
+		UIKit.UILabel DescriptionLabel { get; set; }
+
+		[Outlet]
+		UIKit.UIView DotView { get; set; }
+
+		[Outlet]
+		UIKit.UILabel DurationLabel { get; set; }
+
+		[Outlet]
+		UIKit.UILabel ProjectNameLabel { get; set; }
+
+		[Outlet]
+		UIKit.UIView RunningTimerContainerView { get; set; }
+
+		[Outlet]
+		UIKit.UIButton StartButton { get; set; }
+
+		[Outlet]
+		UIKit.UIButton StopButton { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (DurationLabel != null) {
+				DurationLabel.Dispose ();
+				DurationLabel = null;
+			}
+
+			if (RunningTimerContainerView != null) {
+				RunningTimerContainerView.Dispose ();
+				RunningTimerContainerView = null;
+			}
+
+			if (DescriptionLabel != null) {
+				DescriptionLabel.Dispose ();
+				DescriptionLabel = null;
+			}
+
+			if (ProjectNameLabel != null) {
+				ProjectNameLabel.Dispose ();
+				ProjectNameLabel = null;
+			}
+
+			if (DotView != null) {
+				DotView.Dispose ();
+				DotView = null;
+			}
+
+			if (StartButton != null) {
+				StartButton.Dispose ();
+				StartButton = null;
+			}
+
+			if (StopButton != null) {
+				StopButton.Dispose ();
+				StopButton = null;
+			}
+		}
+	}
 }
