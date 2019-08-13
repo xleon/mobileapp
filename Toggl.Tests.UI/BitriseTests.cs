8d3528d4a1daf2603579202299a12d1c75bb889b
Arturs Eimanis <arturseimanis@gmail.com>
2019-08-13 12:02:17 +0300

    :construction: Remove all UI tests and add 2 simple ones (4 :sanic:)

---
+++ Toggl.Tests.UI/BitriseTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using Toggl.Tests.UI.Extensions;
+using Xamarin.UITest;
+
+namespace Toggl.Tests.UI
+{
+    [TestFixture]
+    public class BitriseTests
+    {
+        private IApp app;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = Configuration.GetApp();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            app.WaitForMainScreen();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            app.WaitForMainScreen();
+        }
+    }
+}
