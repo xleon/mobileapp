f1b9d467ce8d98de24111ee2d6ea5cc23aa661d4
William Barbosa <heytherewill@gmail.com>
2019-08-14 11:34:27 -0300

    :open_book: Add licenses page (#5763)
    * :construction: Update struct for compliance
    
    * :open_book: Add Licenses Activity
    
    * :art: Add Licenses button back to About page
    
    * :triangular_ruler:
    
    Co-Authored-By: Juan Laube <juanlaube@users.noreply.github.com>

---
+++ Toggl.Droid/Extensions/Reactive/ToolbarExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Android.Support.V7.Widget;
+using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using Toggl.Core.UI.Reactive;
+using Toggl.Shared.Extensions;
+
+namespace Toggl.Droid.Extensions.Reactive
+{
+    public static class ToolbarExtensions
+    {
+        public static IObservable<Unit> NavigationTapped(this IReactive<Toolbar> reactive)
+            => Observable
+                .FromEventPattern<Toolbar.NavigationClickEventArgs>(
+                    e => reactive.Base.NavigationClick += e,
+                    e => reactive.Base.NavigationClick -= e)
+                .SelectUnit();
+    }
+}
