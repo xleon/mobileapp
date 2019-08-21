0c9c7266f4bc37f34394c8b5a28739c375ac4e0f
Daivid S. Silverio <daivid.silverio@gmail.com>
2019-08-21 10:49:03 -0400

    :bug: Fix missing beginning of week translation (#5789)
    * :sparkles: Add helper to translate beginning of week
    
    * :bug: Localize beginning of week
    
    * :white_check_mark: Test ToLocalizedString extension

--- Toggl.Core.UI/ViewModels/Settings/SettingsViewModel.cs
+++ Toggl.Core.UI/ViewModels/Settings/SettingsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Reactive;
 using System.Reactive.Disposables;
@@ -182,7 +183,7 @@ namespace Toggl.Core.UI.ViewModels
                 dataSource.User.Current
                     .Select(user => user.BeginningOfWeek)
                     .DistinctUntilChanged()
-                    .Select(beginningOfWeek => beginningOfWeek.ToString())
+                    .Select(beginningOfWeek => beginningOfWeek.ToLocalizedString())
                     .AsDriver(schedulerProvider);
 
             DateFormat =
---
+++ Toggl.Shared.Tests/Extensions/BeginningOfWeekExtensionsTests.cs
@@ -0,0 +1,40 @@
+using FluentAssertions;
+using System.Globalization;
+using Toggl.Shared.Extensions;
+using Xunit;
+
+namespace Toggl.Shared.Tests
+{
+    public sealed class BeginningOfWeekExtensionsTests
+    {
+        [Theory, LogIfTooSlow]
+        [InlineData(BeginningOfWeek.Monday, "月曜日")]
+        [InlineData(BeginningOfWeek.Tuesday, "火曜日")]
+        [InlineData(BeginningOfWeek.Wednesday, "水曜日")]
+        [InlineData(BeginningOfWeek.Thursday, "木曜日")]
+        [InlineData(BeginningOfWeek.Friday, "金曜日")]
+        [InlineData(BeginningOfWeek.Saturday, "土曜日")]
+        [InlineData(BeginningOfWeek.Sunday, "日曜日")]
+        public void LocalizesToJapaneseProperly(BeginningOfWeek beginningOfWeek, string translation)
+        {
+            CultureInfo.CurrentCulture = new CultureInfo("ja");
+
+            beginningOfWeek.ToLocalizedString().Should().Be(translation);
+        }
+        
+        [Theory, LogIfTooSlow]
+        [InlineData(BeginningOfWeek.Monday, "Monday")]
+        [InlineData(BeginningOfWeek.Tuesday, "Tuesday")]
+        [InlineData(BeginningOfWeek.Wednesday, "Wednesday")]
+        [InlineData(BeginningOfWeek.Thursday, "Thursday")]
+        [InlineData(BeginningOfWeek.Friday, "Friday")]
+        [InlineData(BeginningOfWeek.Saturday, "Saturday")]
+        [InlineData(BeginningOfWeek.Sunday, "Sunday")]
+        public void LocalizesToEnglishProperly(BeginningOfWeek beginningOfWeek, string translation)
+        {
+            CultureInfo.CurrentCulture = new CultureInfo("en");
+
+            beginningOfWeek.ToLocalizedString().Should().Be(translation);
+        }
+    }
+}
