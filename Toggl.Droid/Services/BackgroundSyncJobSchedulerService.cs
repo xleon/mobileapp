001a41054f47f5d0d2ee69543ee9963c1f560b67
William Barbosa <heytherewill@gmail.com>
2019-11-12 10:15:30 -0300

    :recycle: Update widgets when running BG sync (#6516)

--- Toggl.Droid/Services/BackgroundSyncJobSchedulerService.cs
+++ Toggl.Droid/Services/BackgroundSyncJobSchedulerService.cs
@@ -30,7 +30,12 @@ namespace Toggl.Droid.Services
             var dependencyContainer = AndroidDependencyContainer.Instance;
             if (!dependencyContainer.UserAccessManager.CheckIfLoggedIn())
                 return false;
-            
+
+            // The widgets service will listen for changes to the running
+            // time entry and it will update the data in the shared database
+            // and that way the widget will show correct information after we sync.
+            dependencyContainer.WidgetsService.Start();
+
             disposable = dependencyContainer.InteractorFactory.RunBackgroundSync()
                 .Execute()
                 .Subscribe(DoNothing, DoNothing,
